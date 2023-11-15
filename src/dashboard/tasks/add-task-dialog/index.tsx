8b08e31d32e3458ddca446facd77c6966812afda
danhalf <danhel@rocketmail.com>
2023-11-15 19:35:29 +0200

    init

--- src/dashboard/tasks/add-task-dialog/index.tsx
+++ src/dashboard/tasks/add-task-dialog/index.tsx
@@ -1,1 +1,14 @@
-export const AddTaskDialog = () => {};
+import { Dialog, DialogProps } from "primereact/dialog";
+
+export const AddTaskDialog = ({ visible, onHide }: DialogProps) => {
+    return (
+        <div className='card flex justify-content-center'>
+            <Dialog
+                header='Header'
+                visible={visible}
+                style={{ width: "50vw" }}
+                onHide={onHide}
+            ></Dialog>
+        </div>
+    );
+};
--- src/dashboard/tasks/index.tsx
+++ src/dashboard/tasks/index.tsx
@@ -2,15 +2,25 @@ import { useEffect, useState } from "react";
 import { AuthUser } from "http/services/auth.service";
 import { getKeyValue } from "services/local-storage.service";
 import { Task, getTasksByUserId } from "http/services/tasks.service";
+import { AddTaskDialog } from "./add-task-dialog";
 
 export const Tasks = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [showAddTaskDialog, setShowAddTaskDialog] = useState<boolean>(false);
     useEffect(() => {
         const authUser: AuthUser = getKeyValue("admss-client-app-user");
         if (authUser) {
             getTasksByUserId(authUser.useruid).then((response) => setTasks(response.splice(0, 5)));
         }
     }, []);
+
+    const handleAddTaskClick = () => {
+        setShowAddTaskDialog(true);
+    };
+
+    const handleAddTaskDialogHide = () => {
+        setShowAddTaskDialog(false);
+    };
     return (
         <>
             <h2 className='card-content__title uppercase'>Tasks</h2>
@@ -25,10 +35,16 @@ export const Tasks = () => {
                     </li>
                 ))}
             </ul>
-            <span className='add-task-control font-semibold cursor-pointer'>
+            <span
+                className='add-task-control font-semibold cursor-pointer'
+                onClick={handleAddTaskClick}
+            >
                 <i className='pi pi-plus add-task-control__icon'></i>
                 Add new task
             </span>
+            <div className='hidden'>
+                <AddTaskDialog visible={showAddTaskDialog} onHide={handleAddTaskDialogHide} />
+            </div>
         </>
     );
 };
