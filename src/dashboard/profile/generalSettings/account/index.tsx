4d272252ed93e6a03e9a70910726eecd545c267d
danhalf <danhel@rocketmail.com>
2024-01-02 19:51:29 +0200

    added: contract settings

--- src/dashboard/profile/generalSettings/account/index.tsx
+++ src/dashboard/profile/generalSettings/account/index.tsx
@@ -9,16 +9,17 @@ interface SettingsAccountProps {
 }
 
 export const SettingsAccount = ({ settings }: SettingsAccountProps) => {
+    const [valueDigits, setValueDigits] = useState<number>(2);
     const [value, setValue] = useState<number>(5);
     return (
         <div className='account flex flex-column gap-4'>
-            <div className='text-lg pb-4 font-semibold'>Account Settings</div>
+            <div className='text-lg font-semibold'>Account Settings</div>
             <div className='flex justify-content-between align-items-center'>
                 <label htmlFor='account-start-number' className='start-number__label ml-2 wrap'>
                     Start number (starts from 0 by default)
                 </label>
                 <InputText
-                    value={String(value)}
+                    value={"0"}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => {}}
                     id='account-start-number'
                     className='start-number__input'
@@ -30,45 +31,49 @@ export const SettingsAccount = ({ settings }: SettingsAccountProps) => {
                 </label>
                 <div className='flex-1 ml-8'>
                     <InputText
-                        value={String(value)}
+                        value={String(valueDigits)}
                         onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                            setValue(Number(e.target.value))
+                            setValueDigits(Number(e.target.value))
                         }
                         className='w-full'
                     />
                     <Slider
-                        value={value}
-                        onChange={(e: SliderChangeEvent) => setValue(Number(e.value))}
+                        value={valueDigits}
+                        onChange={(e: SliderChangeEvent) => setValueDigits(Number(e.value))}
                         max={10}
                         className='w-full'
                     />
                 </div>
             </div>
             <div className='flex align-items-center justify-content-between'>
-                <span className='p-float-label'>
+                <span className='p-float-label '>
                     <InputText id='account-prefix' className='account__input' />
                     <label htmlFor='account-prefix'>Prefix</label>
                 </span>
-                <span className='p-float-label'>
+                <span className='p-float-label '>
                     <InputText id='account-suffix' className='account__input' />
                     <label htmlFor='account-suffix'>Suffix</label>
                 </span>
             </div>
             <div className='flex align-items-center justify-content-between'>
-                <CurrencyInput title='Late fee (min)' />
-                <CurrencyInput title='Late fee (max)' />
+                <div className='account__late-fee'>
+                    <CurrencyInput title='Late fee (min)' />
+                </div>
+                <div className='account__late-fee'>
+                    <CurrencyInput title='Late fee (max)' />
+                </div>
             </div>
-            <div className='flex'>
-                <label htmlFor={settings} className='ml-2 wrap'>
+            <div className='flex align-items-center justify-content-between account-grace'>
+                <label htmlFor={settings} className='account-grace__label'>
                     Late fee grace period
                 </label>
-                <div className='flex-1 ml-8'>
+                <div className='account-grace__input'>
                     <InputText
+                        className='account-grace__input'
                         value={String(value)}
                         onChange={(e: ChangeEvent<HTMLInputElement>) =>
                             setValue(Number(e.target.value))
                         }
-                        className='w-full'
                     />
                     <Slider
                         value={value}
@@ -78,8 +83,8 @@ export const SettingsAccount = ({ settings }: SettingsAccountProps) => {
                     />
                 </div>
             </div>
-            <div className='w-6'>
-                <PercentInput title='Late fee percentage' />
+            <div className='account-percentage'>
+                <PercentInput className='account-percentage__input' title='Late fee percentage' />
             </div>
         </div>
     );
