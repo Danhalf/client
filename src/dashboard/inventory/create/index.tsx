7b7f643375698d9bcbf4381f8cac2714f40011ec
Danhalf <danhel@rocketmail.com>
2024-01-05 19:00:07 +0200

    added: accordion auto toggle

--- src/dashboard/inventory/create/index.tsx
+++ src/dashboard/inventory/create/index.tsx
@@ -1,7 +1,7 @@
 import { Steps } from "primereact/steps";
 import { MenuItem, MenuItemOptions } from "primereact/menuitem";
 import "./index.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Accordion, AccordionTab } from "primereact/accordion";
 import { Button } from "primereact/button";
 import { InventoryVehicle } from "./vehicle";
@@ -29,41 +29,71 @@ const newTemplate = (item: MenuItem, options: MenuItemOptions) => {
     );
 };
 
-const items: MenuItem[] = [
-    {
-        label: "General",
-    },
-    {
-        label: "Description",
-    },
-    {
-        label: "Options",
-    },
-    {
-        label: "Checks",
-    },
-    {
-        label: "Inspections",
-    },
-    {
-        label: "Keys",
-    },
-    {
-        label: "Disclosures",
-    },
-    {
-        label: "Other",
-    },
+const vehicleLabels = [
+    "General",
+    "Description",
+    "Options",
+    "Checks",
+    "Inspections",
+    "Keys",
+    "Disclosures",
+    "Other",
 ];
+const purchaseLabels = ["Floorplan", "Consign", "Title", "Purchases", "Expenses", "Payments"];
+const mediaDataLabels = ["Images", "Video", "Audio", "Documents"];
 
-const itemsWithTemplate = items.map((item: MenuItem) => ({
-    ...item,
-    template: (item: MenuItem, options: MenuItemOptions) => newTemplate(item, options),
-}));
+const vehicleItems: MenuItem[] = vehicleLabels.map((label) => {
+    return {
+        label,
+        template: (item: MenuItem, options: MenuItemOptions) => newTemplate(item, options),
+    };
+});
+const purchaseItems: MenuItem[] = purchaseLabels.map((label) => {
+    return {
+        label,
+        template: (item: MenuItem, options: MenuItemOptions) => newTemplate(item, options),
+    };
+});
+const mediaDataItems: MenuItem[] = mediaDataLabels.map((label) => {
+    return {
+        label,
+        template: (item: MenuItem, options: MenuItemOptions) => newTemplate(item, options),
+    };
+});
+
+const accordionFirstStep = vehicleItems.length;
+const accordionSecondStep = accordionFirstStep + purchaseItems.length;
 
 export const CreateInventory = () => {
     const [stepActiveIndex, setStepActiveIndex] = useState<number>(0);
-    const [accordionActiveIndex, setAccordionActiveIndex] = useState<number>(0);
+    const [accordionActiveIndex, setAccordionActiveIndex] = useState<number | number[]>([0]);
+
+    useEffect(() => {
+        if (Array.isArray(accordionActiveIndex)) {
+            if (stepActiveIndex > accordionSecondStep - 1) {
+                return setAccordionActiveIndex((prev) => {
+                    const updatedArray = [...(prev as number[])];
+                    updatedArray[2] = 2;
+                    return updatedArray;
+                });
+            }
+            if (stepActiveIndex > accordionFirstStep - 1) {
+                return setAccordionActiveIndex((prev) => {
+                    const updatedArray = [...(prev as number[])];
+                    updatedArray[1] = 1;
+                    return updatedArray;
+                });
+            }
+        }
+        return setAccordionActiveIndex((prev) => {
+            const updatedArray = [...(prev as number[])];
+            updatedArray[0] = 0;
+            return updatedArray;
+        });
+    }, [stepActiveIndex]);
+
+    // eslint-disable-next-line no-console
+    console.log(accordionActiveIndex);
 
     return (
         <div className='grid'>
@@ -77,11 +107,13 @@ export const CreateInventory = () => {
                             <div className='col-4'>
                                 <Accordion
                                     activeIndex={accordionActiveIndex}
+                                    onTabChange={(e) => setAccordionActiveIndex(e.index)}
                                     className='create-inventory__accordion'
+                                    multiple
                                 >
                                     <AccordionTab pt={{}} header='Vehicle'>
                                         <Steps
-                                            model={itemsWithTemplate}
+                                            model={vehicleItems}
                                             activeIndex={stepActiveIndex}
                                             className='vertical-step-menu'
                                             pt={{
@@ -93,38 +125,43 @@ export const CreateInventory = () => {
                                             }}
                                         />
                                     </AccordionTab>
-                                    <AccordionTab header='Purchase'></AccordionTab>
-                                    <AccordionTab header='Media data'></AccordionTab>
+                                    <AccordionTab header='Purchase'>
+                                        <Steps
+                                            model={purchaseItems}
+                                            activeIndex={stepActiveIndex - accordionFirstStep}
+                                            className='vertical-step-menu'
+                                            pt={{
+                                                menu: { className: "flex-column w-full" },
+                                                step: {
+                                                    className:
+                                                        "border-circle create-inventory-step",
+                                                },
+                                            }}
+                                        />
+                                    </AccordionTab>
+                                    <AccordionTab header='Media data'>
+                                        <Steps
+                                            model={mediaDataItems}
+                                            activeIndex={stepActiveIndex - accordionSecondStep}
+                                            className='vertical-step-menu'
+                                            pt={{
+                                                menu: { className: "flex-column w-full" },
+                                                step: {
+                                                    className:
+                                                        "border-circle create-inventory-step",
+                                                },
+                                            }}
+                                        />
+                                    </AccordionTab>
                                 </Accordion>
                             </div>
                             <div className='col-8 flex flex-column'>
-                                <InventoryVehicle />
+                                <InventoryVehicle
+                                    itemsLength={vehicleItems.length - 1}
+                                    menuIndex={stepActiveIndex}
+                                    setMenuIndex={setStepActiveIndex}
+                                />
                             </div>
-                        </div>
-                        <div className='flex justify-content-end gap-3'>
-                            <Button
-                                onClick={() => setStepActiveIndex((prev) => --prev)}
-                                disabled={!stepActiveIndex}
-                                className='uppercase  px-6'
-                                outlined
-                            >
-                                Back
-                            </Button>
-                            <Button
-                                onClick={() => setStepActiveIndex((prev) => ++prev)}
-                                disabled={stepActiveIndex >= items.length - 1}
-                                className='uppercase px-6'
-                                outlined
-                            >
-                                Next
-                            </Button>
-                            <Button
-                                onClick={() => setStepActiveIndex((prev) => ++prev)}
-                                disabled={stepActiveIndex !== items.length - 1}
-                                className='uppercase px-6'
-                            >
-                                Save
-                            </Button>
                         </div>
                     </div>
                 </div>
--- src/dashboard/inventory/create/vehicle/index.tsx
+++ src/dashboard/inventory/create/vehicle/index.tsx
@@ -1,8 +1,45 @@
+import { Button } from "primereact/button";
 import "./index.css";
-export const InventoryVehicle = () => {
+
+interface InventoryVehicleProps {
+    menuIndex: number;
+    itemsLength: number;
+    setMenuIndex: (index: number) => void;
+}
+
+export const InventoryVehicle = ({
+    menuIndex,
+    itemsLength,
+    setMenuIndex,
+}: InventoryVehicleProps): JSX.Element => {
     return (
         <>
             <div className='flex flex-grow-1'>General</div>
+            <div className='flex justify-content-end gap-3'>
+                <Button
+                    onClick={() => setMenuIndex(menuIndex - 1)}
+                    disabled={!menuIndex}
+                    className='uppercase  px-6'
+                    outlined
+                >
+                    Back
+                </Button>
+                <Button
+                    onClick={() => setMenuIndex(menuIndex + 1)}
+                    //   disabled={menuIndex >= itemsLength}
+                    className='uppercase px-6'
+                    outlined
+                >
+                    Next
+                </Button>
+                <Button
+                    onClick={() => {}}
+                    disabled={menuIndex !== itemsLength}
+                    className='uppercase px-6'
+                >
+                    Save
+                </Button>
+            </div>
         </>
     );
 };
