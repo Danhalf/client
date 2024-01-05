022b4d861de9aa8090417b2ff8a190497f3e33c6
danhalf <danhel@rocketmail.com>
2024-01-05 22:34:02 +0200

    saved: recoded by class

--- src/dashboard/inventory/create/index.tsx
+++ src/dashboard/inventory/create/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Steps } from "primereact/steps";
 import { MenuItem, MenuItemOptions } from "primereact/menuitem";
 import "./index.css";
@@ -6,110 +7,115 @@ import { Accordion, AccordionTab } from "primereact/accordion";
 import { Button } from "primereact/button";
 import { InventoryVehicle } from "./vehicle";
 
-const newTemplate = (item: MenuItem, options: MenuItemOptions) => {
-    return (
-        // eslint-disable-next-line jsx-a11y/anchor-is-valid
-        <a
-            href='#'
-            className={`${options.className} vertical-nav flex-row align-items-center justify-content-start w-full`}
-            role='presentation'
-            data-pc-section='action'
-        >
-            <span
-                className={"vertical-nav__icon p-steps-number border-circle "}
-                data-pc-section='step'
-            ></span>
-            <span
-                className={`${options.labelClassName} vertical-nav__label`}
-                data-pc-section='label'
-            >
-                {item.label}
-            </span>
-        </a>
-    );
-};
-
-const sections = [
-    {
-        label: "Vehicle",
-        items: [
-            "General",
-            "Description",
-            "Options",
-            "Checks",
-            "Inspections",
-            "Keys",
-            "Disclosures",
-            "Other",
-        ],
-    },
-    {
-        label: "Purchase",
-        items: ["Floorplan", "Consign", "Title", "Purchases", "Expenses", "Payments"],
-    },
-    {
-        label: "Media data",
-        items: ["Images", "Video", "Audio", "Documents"],
-    },
-];
-
 interface MenuItemProps extends MenuItem {
-    sectionIndex: number;
-    index: number;
-    startIndex: number;
+    itemIndex: number;
 }
 
-let itemsLength: number = 0;
-const allItems: MenuItemProps[] = sections.flatMap((section, index) => {
-    const itemIndex = () => itemsLength++;
-    return section.items.map((label) => ({
-        label,
-        template: (item: MenuItem, options: MenuItemOptions) => newTemplate(item, options),
-        sectionIndex: index,
-        index: itemIndex(),
-        startIndex: sections[index].items.length,
-    }));
+interface InventorySection {
+    label: string;
+    items: MenuItemProps[];
+    sectionId?: number;
+    startIndex: number;
+    getLength?: () => number;
+}
+
+class AccordionSection implements InventorySection {
+    static instancesCount: number = 0;
+    static itemIndex: number = 0;
+    sectionId: number;
+    label: string;
+    startIndex: number = 0;
+    items: MenuItemProps[];
+
+    constructor({ label, items }: { label: string; items: string[] }) {
+        this.sectionId = ++AccordionSection.instancesCount;
+        this.label = label;
+        this.items = items.map((label) => ({
+            label,
+            itemIndex: AccordionSection.itemIndex++,
+            template: (item: MenuItem, options: MenuItemOptions) => this.newTemplate(item, options),
+        }));
+        this.startIndex = AccordionSection.itemIndex - this.items.length;
+    }
+
+    private newTemplate(item: MenuItem, options: MenuItemOptions): JSX.Element {
+        return (
+            <a
+                href='#'
+                className={`${options.className} vertical-nav flex-row align-items-center justify-content-start w-full`}
+                role='presentation'
+                data-pc-section='action'
+            >
+                <span
+                    className={"vertical-nav__icon p-steps-number border-circle "}
+                    data-pc-section='step'
+                ></span>
+                <span
+                    className={`${options.labelClassName} vertical-nav__label`}
+                    data-pc-section='label'
+                >
+                    {item.label}
+                </span>
+            </a>
+        );
+    }
+
+    public getLength(): number {
+        return this.items.length;
+    }
+}
+
+const vehicleSection = new AccordionSection({
+    label: "Vehicle",
+    items: [
+        "General",
+        "Description",
+        "Options",
+        "Checks",
+        "Inspections",
+        "Keys",
+        "Disclosures",
+        "Other",
+    ],
 });
 
-// const getStartActiveIndex = (sectionIndex: number): number => {
-//     // eslint-disable-next-line no-console
-//     console.log(sectionIndex, sections[sectionIndex]);
-//     return 0;
-//     // return allItems
-//     //     .slice(0, sectionIndex)
-//     //     .reduce((acc, section) => acc + section.items.length, 0);
-// };
+const purchaseSection = new AccordionSection({
+    label: "Purchase",
+    items: ["Floorplan", "Consign", "Title", "Purchases", "Expenses", "Payments"],
+});
 
-// eslint-disable-next-line no-console
-console.log(allItems[3].startIndex);
+const mediaSection = new AccordionSection({
+    label: "Media data",
+    items: ["Images", "Video", "Audio", "Documents"],
+});
 
 export const CreateInventory = () => {
     const [stepActiveIndex, setStepActiveIndex] = useState<number>(0);
     const [accordionActiveIndex, setAccordionActiveIndex] = useState<number | number[]>([0]);
 
-    // useEffect(() => {
-    //     if (Array.isArray(accordionActiveIndex)) {
-    //         if (stepActiveIndex > accordionSecondStep - 1) {
-    //             return setAccordionActiveIndex((prev) => {
-    //                 const updatedArray = [...(prev as number[])];
-    //                 updatedArray[2] = 2;
-    //                 return updatedArray;
-    //             });
-    //         }
-    //         if (stepActiveIndex > accordionFirstStep - 1) {
-    //             return setAccordionActiveIndex((prev) => {
-    //                 const updatedArray = [...(prev as number[])];
-    //                 updatedArray[1] = 1;
-    //                 return updatedArray;
-    //             });
-    //         }
-    //     }
-    //     return setAccordionActiveIndex((prev) => {
-    //         const updatedArray = [...(prev as number[])];
-    //         updatedArray[0] = 0;
-    //         return updatedArray;
-    //     });
-    // }, [stepActiveIndex]);
+    useEffect(() => {
+        //     if (Array.isArray(accordionActiveIndex)) {
+        //         if (stepActiveIndex > accordionSecondStep - 1) {
+        //             return setAccordionActiveIndex((prev) => {
+        //                 const updatedArray = [...(prev as number[])];
+        //                 updatedArray[2] = 2;
+        //                 return updatedArray;
+        //             });
+        //         }
+        //         if (stepActiveIndex > accordionFirstStep - 1) {
+        //             return setAccordionActiveIndex((prev) => {
+        //                 const updatedArray = [...(prev as number[])];
+        //                 updatedArray[1] = 1;
+        //                 return updatedArray;
+        //             });
+        //         }
+        //     }
+        //     return setAccordionActiveIndex((prev) => {
+        //         const updatedArray = [...(prev as number[])];
+        //         updatedArray[0] = 0;
+        //         return updatedArray;
+        //     });
+    }, [stepActiveIndex]);
 
     return (
         <div className='grid'>
@@ -127,29 +133,57 @@ export const CreateInventory = () => {
                                     className='create-inventory__accordion'
                                     multiple
                                 >
-                                    {sections.map((section, index) => (
-                                        <AccordionTab key={section.label} header={section.label}>
-                                            <Steps
-                                                model={allItems.filter(
-                                                    (item) => item.sectionIndex === index
-                                                )}
-                                                activeIndex={stepActiveIndex}
-                                                className='vertical-step-menu'
-                                                pt={{
-                                                    menu: { className: "flex-column w-full" },
-                                                    step: {
-                                                        className:
-                                                            "border-circle create-inventory-step",
-                                                    },
-                                                }}
-                                            />
-                                        </AccordionTab>
-                                    ))}
+                                    <AccordionTab header={vehicleSection.label}>
+                                        <Steps
+                                            model={vehicleSection.items}
+                                            activeIndex={
+                                                stepActiveIndex - vehicleSection.startIndex
+                                            }
+                                            className='vertical-step-menu'
+                                            pt={{
+                                                menu: { className: "flex-column w-full" },
+                                                step: {
+                                                    className:
+                                                        "border-circle create-inventory-step",
+                                                },
+                                            }}
+                                        />
+                                    </AccordionTab>
+                                    <AccordionTab header={purchaseSection.label}>
+                                        <Steps
+                                            model={purchaseSection.items}
+                                            activeIndex={
+                                                stepActiveIndex - purchaseSection.startIndex
+                                            }
+                                            className='vertical-step-menu'
+                                            pt={{
+                                                menu: { className: "flex-column w-full" },
+                                                step: {
+                                                    className:
+                                                        "border-circle create-inventory-step",
+                                                },
+                                            }}
+                                        />
+                                    </AccordionTab>
+                                    <AccordionTab header={mediaSection.label}>
+                                        <Steps
+                                            model={mediaSection.items}
+                                            activeIndex={stepActiveIndex - mediaSection.startIndex}
+                                            className='vertical-step-menu'
+                                            pt={{
+                                                menu: { className: "flex-column w-full" },
+                                                step: {
+                                                    className:
+                                                        "border-circle create-inventory-step",
+                                                },
+                                            }}
+                                        />
+                                    </AccordionTab>
                                 </Accordion>
                             </div>
                             <div className='col-8 flex flex-column'>
                                 <InventoryVehicle
-                                    // itemsLength={vehicleItems.length - 1}
+                                    itemsLength={vehicleSection.getLength() - 1}
                                     menuIndex={stepActiveIndex}
                                     setMenuIndex={setStepActiveIndex}
                                 />
