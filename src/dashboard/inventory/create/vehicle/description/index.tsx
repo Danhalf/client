d4ae8cc431b5e3645d466bc1cddeb253abc481bc
danhalf <danhel@rocketmail.com>
2024-01-09 08:12:43 +0200

    added: description

--- src/dashboard/inventory/create/vehicle/description/index.tsx
+++ src/dashboard/inventory/create/vehicle/description/index.tsx
@@ -1,7 +1,45 @@
+import { Dropdown } from "primereact/dropdown";
+import { InputText } from "primereact/inputtext";
+
 export const VehicleDescription = (): JSX.Element => {
     return (
-        <>
-            <div className='flex flex-grow-1'>description</div>
-        </>
+        <div className='grid vehicle-description row-gap-2'>
+            <div className='col-6'>
+                <Dropdown
+                    optionLabel='name'
+                    placeholder='Transmission'
+                    className='w-full vehicle-description__dropdown'
+                />
+            </div>
+
+            <div className='col-6 flex justify-content-between gap-2'>
+                <Dropdown
+                    optionLabel='name'
+                    placeholder='Type of Fuel (required)'
+                    className='w-6 vehicle-description__dropdown'
+                />
+                <Dropdown
+                    optionLabel='name'
+                    placeholder='Drive Line'
+                    className='w-6 vehicle-description__dropdown'
+                />
+            </div>
+
+            <div className='col-4'>
+                <Dropdown
+                    optionLabel='name'
+                    placeholder='Drive Line'
+                    className='w-full vehicle-description__dropdown'
+                />
+            </div>
+
+            <div className='col-8'>
+                <Dropdown
+                    optionLabel='name'
+                    placeholder='Interior color'
+                    className='w-full vehicle-description__dropdown'
+                />
+            </div>
+        </div>
     );
 };
