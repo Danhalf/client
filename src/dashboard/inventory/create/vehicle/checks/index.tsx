577337f95d961e2789f11eb6116afccf3a0c5b90
Danhalf <danhel@rocketmail.com>
2024-01-09 11:50:03 +0200

    added: check form

--- src/dashboard/inventory/create/vehicle/checks/index.tsx
+++ src/dashboard/inventory/create/vehicle/checks/index.tsx
@@ -1,7 +1,78 @@
+import { Checkbox } from "primereact/checkbox";
+import { useState } from "react";
+import "./index.css";
+
 export const VehicleChecks = (): JSX.Element => {
+    const topChecks = [
+        { name: "AutoCheck done", value: 1 },
+        { name: "State inspection Performed", value: 1 },
+        { name: "Oil and Filter inspected and changed", value: 0 },
+    ];
+    const bottomChecks = [
+        { name: "User defined inspection", value: 1 },
+        { name: "User defined inspection", value: 0 },
+        { name: "User defined inspection", value: 1 },
+        { name: "User defined inspection", value: 0 },
+        { name: "User defined inspection", value: 1 },
+        { name: "User defined inspection", value: 0 },
+        { name: "User defined inspection", value: 1 },
+        { name: "User defined inspection", value: 0 },
+        { name: "User defined inspection", value: 1 },
+        { name: "User defined inspection", value: 0 },
+    ];
+    const [selectedTopCheck, setSelectedTopCheck] = useState(
+        topChecks.filter((topCheck) => topCheck.value === 1)
+    );
+    const [selectedBottomCheck, setSelectedBottomCheck] = useState(
+        bottomChecks.filter((bottomCheck) => bottomCheck.value === 1)
+    );
+
+    const onTopCheckChange = (e: any) => {
+        const updatedTopCheck = selectedTopCheck.includes(e.value)
+            ? selectedTopCheck.filter((option) => option !== e.value)
+            : [...selectedTopCheck, e.value];
+
+        setSelectedTopCheck(updatedTopCheck);
+    };
+
     return (
-        <>
-            <div className='flex flex-grow-1'>Checks</div>
-        </>
+        <div className='grid flex-column vehicle-checks'>
+            <div className='grid flex-column vehicle-checks__top'>
+                {topChecks.map((topCheck) => (
+                    <div
+                        key={topCheck.name}
+                        className='vehicle-checks__checkbox flex align-items-center'
+                    >
+                        <Checkbox
+                            inputId={topCheck.name}
+                            name='topCheck'
+                            onChange={onTopCheckChange}
+                            checked={selectedTopCheck.includes(topCheck)}
+                        />
+                        <label htmlFor={topCheck.name} className='ml-2'>
+                            {topCheck.name}
+                        </label>
+                    </div>
+                ))}
+            </div>
+            <div className='grid flex-column vehicle-checks__bottom'>
+                {bottomChecks.map((bottomCheck) => (
+                    <div
+                        key={bottomCheck.name}
+                        className='vehicle-checks__checkbox flex align-items-center'
+                    >
+                        <Checkbox
+                            inputId={bottomCheck.name}
+                            name='bottomCheck'
+                            onChange={onTopCheckChange}
+                            checked={selectedBottomCheck.includes(bottomCheck)}
+                        />
+                        <label htmlFor={bottomCheck.name} className='ml-2'>
+                            {bottomCheck.name}
+                        </label>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
--- src/dashboard/inventory/create/vehicle/options/index.tsx
+++ src/dashboard/inventory/create/vehicle/options/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 
 export const VehicleOptions = (): JSX.Element => {
     const options = [
+        { name: "Select All", value: 0 },
         { name: "A/C", value: 0 },
         { name: "AM/FM Stereo", value: 0 },
         { name: "All Wheel Drive", value: 0 },
@@ -13,16 +14,16 @@ export const VehicleOptions = (): JSX.Element => {
         { name: "Anti-Skid Control", value: 1 },
         { name: "Apple Car Play", value: 1 },
         { name: "Automatic Climate Control", value: 1 },
-        { name: "CD Changer", value: 1 },
+        { name: "CD Changer", value: 0 },
         { name: "Cassette Player", value: 1 },
         { name: "Cruise Control", value: 1 },
         { name: "Fog Lights", value: 1 },
-        { name: "Four Wheel Drive", value: 1 },
+        { name: "Four Wheel Drive", value: 0 },
         { name: "Leather Upholstry", value: 1 },
         { name: "MoonRoof", value: 1 },
         { name: "Navigation System", value: 1 },
         { name: "Overhead Console", value: 1 },
-        { name: "Power  Steering", value: 1 },
+        { name: "Power  Steering", value: 0 },
         { name: "Power Door Locks", value: 1 },
         { name: "Power Seats", value: 1 },
         { name: "Power Windows", value: 1 },
@@ -30,45 +31,45 @@ export const VehicleOptions = (): JSX.Element => {
         { name: "Rear Window Wiper", value: 1 },
         { name: "Remote Keyless Entry", value: 1 },
         { name: "Sun Roof", value: 1 },
-        { name: "Sunscreen Glass", value: 1 },
+        { name: "Sunscreen Glass", value: 0 },
         { name: "Tinted Windows", value: 1 },
         { name: "Convertable Top", value: 1 },
-        { name: "User  Defined1", value: 1 },
-        { name: "User  Defined2", value: 1 },
-        { name: "User  Defined3", value: 1 },
-        { name: "User  Defined4", value: 1 },
+        { name: "User  Defined1", value: 0 },
+        { name: "User  Defined2", value: 0 },
+        { name: "User  Defined3", value: 0 },
+        { name: "User  Defined4", value: 0 },
     ];
-    const [selectedOptions, setSelectedOptions] = useState([options[1]]);
+    const [selectedOptions, setSelectedOptions] = useState(
+        options.filter((option) => option.value === 1)
+    );
 
-    const onCategoryChange = (e: any) => {
-        let _selectedOptions = [...selectedOptions];
+    const onOptionsChange = (e: any) => {
+        const updatedOptions = selectedOptions.includes(e.value)
+            ? selectedOptions.filter((option) => option !== e.value)
+            : [...selectedOptions, e.value];
 
-        if (e.checked) _selectedOptions.push(e.value);
-        else _selectedOptions = _selectedOptions.filter((option) => option.value !== e.value.value);
-
-        setSelectedOptions(_selectedOptions);
+        setSelectedOptions(updatedOptions);
     };
+
     return (
         <>
             <div className='grid flex-column vehicle-options'>
-                {options.map((option) => {
-                    return (
-                        <div key={option.name} className='vehicle-options__checkbox'>
-                            <Checkbox
-                                inputId={option.name}
-                                name='option'
-                                value={option}
-                                onChange={onCategoryChange}
-                                checked={selectedOptions.some(
-                                    (item: any) => item.key === option.value
-                                )}
-                            />
-                            <label htmlFor={option.name} className='ml-2'>
-                                {option.name}
-                            </label>
-                        </div>
-                    );
-                })}
+                {options.map((option) => (
+                    <div
+                        key={option.name}
+                        className='vehicle-options__checkbox flex align-items-center'
+                    >
+                        <Checkbox
+                            inputId={option.name}
+                            name='option'
+                            onChange={onOptionsChange}
+                            checked={selectedOptions.includes(option)}
+                        />
+                        <label htmlFor={option.name} className='ml-2'>
+                            {option.name}
+                        </label>
+                    </div>
+                ))}
             </div>
         </>
     );
