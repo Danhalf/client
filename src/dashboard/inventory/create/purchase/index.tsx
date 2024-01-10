141d4181dc1bd7f40a18790e979c7e3d3120192b
danhalf <danhel@rocketmail.com>
2024-01-10 23:42:52 +0200

    added: consign component

--- src/dashboard/inventory/create/purchase/index.tsx
+++ src/dashboard/inventory/create/purchase/index.tsx
@@ -1,13 +1,14 @@
 import { Inventory } from "dashboard/inventory/common";
 import { PurchaseFloorplan } from "./froorplan";
 import { PurchaseConsign } from "./consign";
+import { PurchaseTitle } from "./title";
 
 export const InventoryPurchaseData: Pick<Inventory, "label" | "items"> = {
     label: "Purchase",
     items: [
         { itemLabel: "Floorplan", component: <PurchaseFloorplan /> },
         { itemLabel: "Consign", component: <PurchaseConsign /> },
-        { itemLabel: "Title" },
+        { itemLabel: "Title", component: <PurchaseTitle /> },
         { itemLabel: "Purchases" },
         { itemLabel: "Expenses" },
         { itemLabel: "Payments" },
