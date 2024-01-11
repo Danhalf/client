4155a31ee530b7aacfde8cdb7f44116f42531990
Danhalf <danhel@rocketmail.com>
2024-01-11 09:06:02 +0200

    changed: vehicle data/checkbox components

--- src/dashboard/inventory/create/vehicle/keys/index.tsx
+++ src/dashboard/inventory/create/vehicle/keys/index.tsx
@@ -1,34 +1,18 @@
 import { ReactElement } from "react";
-import "./index.css";
-import { Checkbox } from "primereact/checkbox";
 import { InputText } from "primereact/inputtext";
+import { BorderedCheckbox } from "dashboard/common/form/inputs";
 
 export const VehicleKeys = (): ReactElement => {
     return (
         <div className='grid vehicle-keys row-gap-2'>
             <div className='col-3'>
-                <div className='p-inputgroup flex-1 w-full align-items-center justify-content-between vehicle-keys__checkbox'>
-                    <label>Keys missing</label>
-                    <span className='p-inputgroup-addon'>
-                        <Checkbox checked={false} />
-                    </span>
-                </div>
+                <BorderedCheckbox name='Keys missing' checked={false} />
             </div>
             <div className='col-3'>
-                <div className='p-inputgroup flex-1 w-full align-items-center justify-content-between vehicle-keys__checkbox'>
-                    <label>Duplicate Keys</label>
-                    <span className='p-inputgroup-addon'>
-                        <Checkbox checked={false} />
-                    </span>
-                </div>
+                <BorderedCheckbox name='Duplicate Keys' checked={false} />
             </div>
             <div className='col-3'>
-                <div className='p-inputgroup flex-1 w-full align-items-center justify-content-between vehicle-keys__checkbox'>
-                    <label>Has Remote</label>
-                    <span className='p-inputgroup-addon'>
-                        <Checkbox checked={false} />
-                    </span>
-                </div>
+                <BorderedCheckbox name='Has Remote' checked={false} />
             </div>
 
             <div className='col-6'>
