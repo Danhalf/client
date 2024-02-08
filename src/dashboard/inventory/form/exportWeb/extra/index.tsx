7ab62a2bde03c4f4e9b8b7674f42e777fbfdb5e0
danhalf <danhel@rocketmail.com>
2024-02-08 18:47:34 +0200

    changed: text-area styles

--- src/dashboard/inventory/form/exportWeb/extra/index.tsx
+++ src/dashboard/inventory/form/exportWeb/extra/index.tsx
@@ -1,49 +1,23 @@
 import { observer } from "mobx-react-lite";
 import { InputTextarea } from "primereact/inputtextarea";
 import { ReactElement } from "react";
+import "./index.css";
 
 export const ExportWebExtra = observer((): ReactElement => {
     return (
         <div className='grid export-web-extra row-gap-2'>
-            <div className='col-12'>
-                <InputTextarea
-                    placeholder='Extra field 1'
-                    className='w-full export-web-extra__text-area'
-                    pt={{
-                        root: {
-                            style: {
-                                height: "100px",
-                            },
-                        },
-                    }}
-                />
-            </div>
-            <div className='col-12'>
-                <InputTextarea
-                    placeholder='Extra field 2'
-                    className='w-full export-web-extra__text-area'
-                    pt={{
-                        root: {
-                            style: {
-                                height: "100px",
-                            },
-                        },
-                    }}
-                />
-            </div>
-            <div className='col-12'>
-                <InputTextarea
-                    placeholder='Extra field 3'
-                    className='w-full export-web-extra__text-area'
-                    pt={{
-                        root: {
-                            style: {
-                                height: "100px",
-                            },
-                        },
-                    }}
-                />
-            </div>
+            <InputTextarea
+                placeholder='Extra field 1'
+                className='w-full export-web-extra__text-area'
+            />
+            <InputTextarea
+                placeholder='Extra field 2'
+                className='w-full export-web-extra__text-area'
+            />
+            <InputTextarea
+                placeholder='Extra field 3'
+                className='w-full export-web-extra__text-area'
+            />
         </div>
     );
 });
--- src/dashboard/inventory/form/exportWeb/links/index.tsx
+++ src/dashboard/inventory/form/exportWeb/links/index.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { InputTextarea } from "primereact/inputtextarea";
 import { ReactElement } from "react";
+import "./index.css";
 
 export const ExportWebLinks = observer((): ReactElement => {
     return (
@@ -8,43 +9,12 @@ export const ExportWebLinks = observer((): ReactElement => {
             <InputTextarea
                 placeholder='VDP Link (Vehicle Detail Page)'
                 className='w-full export-web-links__text-area'
-                pt={{
-                    root: {
-                        style: {
-                            height: "100px",
-                        },
-                    },
-                }}
             />
 
             <hr className='form-line' />
 
-            <div className='col-12'>
-                <InputTextarea
-                    placeholder='Video URL'
-                    className='w-full export-web-links__text-area'
-                    pt={{
-                        root: {
-                            style: {
-                                height: "100px",
-                            },
-                        },
-                    }}
-                />
-            </div>
-            <div className='col-12'>
-                <InputTextarea
-                    placeholder='Photo URL'
-                    className='w-full export-web-links__text-area'
-                    pt={{
-                        root: {
-                            style: {
-                                height: "100px",
-                            },
-                        },
-                    }}
-                />
-            </div>
+            <InputTextarea placeholder='Video URL' className='w-full export-web-links__text-area' />
+            <InputTextarea placeholder='Photo URL' className='w-full export-web-links__text-area' />
         </div>
     );
 });
