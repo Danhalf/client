b94770a2e2cfea74698b0d8b6f7aac5c4a774b2c
Danhalf <danhel@rocketmail.com>
2024-02-13 12:30:31 +0200

    added: modal tail

--- src/dashboard/common/overlay-panel/index.tsx
+++ src/dashboard/common/overlay-panel/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import "./index.css";
 
 import { useRef } from "react";
@@ -8,19 +8,19 @@ interface InfoOverlayPanelProps extends OverlayPanelProps {
     panelTitle?: string;
 }
 export const InfoOverlayPanel = ({ panelTitle, ...props }: InfoOverlayPanelProps): ReactElement => {
-    const op = useRef<OverlayPanel>(null);
+    const [panelShow, setPanelShow] = useState<boolean>(false);
     return (
         <div className='info-panel'>
             <i
-                onClick={(e) => {
-                    op.current?.toggle(e);
-                }}
+                onClick={() => setPanelShow((prev) => !prev)}
                 className='icon adms-help p-text-secondary p-overlay-badge info-panel__icon'
             />
-            <OverlayPanel ref={op} dismissable={false} className='info-panel__panel shadow-3'>
-                <div className='info-panel__title'>{panelTitle}</div>
-                <div className='info-panel__body'>{props.children}</div>
-            </OverlayPanel>
+            {panelShow && (
+                <div className='info-panel__panel shadow-3'>
+                    <div className='info-panel__title'>{panelTitle}</div>
+                    <div className='info-panel__body'>{props.children}</div>
+                </div>
+            )}
         </div>
     );
 };
