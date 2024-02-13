635e3581bcb8e24ddcad92ddd47d2c172243d52b
danhalf <danhel@rocketmail.com>
2024-02-13 19:28:01 +0200

    added: route

---
+++ src/dashboard/export-web/index.tsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from "react";
+import { AuthUser } from "http/services/auth.service";
+import { DatatableQueries, initialDataTableQueries } from "common/models/datatable-queries";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { DataTable, DataTablePageEvent, DataTableSortEvent } from "primereact/datatable";
+import { getKeyValue } from "services/local-storage.service";
+import { QueryParams } from "common/models/query-params";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Column } from "primereact/column";
+import { getDealsList } from "http/services/deals.service";
+import { LS_APP_USER } from "common/constants/localStorage";
+import { ROWS_PER_PAGE } from "common/settings";
+import { Inventory } from "common/models/inventory";
+
+export const ExportToWeb = () => {
+    const [deals, setDeals] = useState<Inventory[]>([]);
+    const [authUser, setUser] = useState<AuthUser | null>(null);
+    const [totalRecords, setTotalRecords] = useState<number>(0);
+    const [globalSearch, setGlobalSearch] = useState<string>("");
+    const [lazyState, setLazyState] = useState<DatatableQueries>(initialDataTableQueries);
+
+    const printTableData = () => {
+        const contactsDoc = new jsPDF();
+        autoTable(contactsDoc, { html: ".p-datatable-table" });
+        contactsDoc.output("dataurlnewwindow");
+    };
+
+    const pageChanged = (event: DataTablePageEvent) => {
+        setLazyState(event);
+    };
+
+    const sortData = (event: DataTableSortEvent) => {
+        setLazyState(event);
+    };
+
+    useEffect(() => {
+        const authUser: AuthUser = getKeyValue(LS_APP_USER);
+        if (authUser) {
+            setUser(authUser);
+            getDealsList(authUser.useruid, { total: 1 }).then((response) => {
+                response && !Array.isArray(response) && setTotalRecords(response.total ?? 0);
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        const params: QueryParams = {
+            ...(lazyState.sortOrder === 1 && { type: "asc" }),
+            ...(lazyState.sortOrder === -1 && { type: "desc" }),
+            ...(globalSearch && { qry: globalSearch }),
+            ...(lazyState.sortField && { column: lazyState.sortField }),
+            skip: lazyState.first,
+            top: lazyState.rows,
+        };
+        if (authUser) {
+            getDealsList(authUser.useruid, params).then((response) => {
+                if (Array.isArray(response)) {
+                    setDeals(response);
+                } else {
+                    setDeals([]);
+                }
+            });
+        }
+    }, [lazyState, authUser, globalSearch]);
+
+    return (
+        <div className='grid'>
+            <div className='col-12'>
+                <div className='card'>
+                    <div className='card-header'>
+                        <h2 className='card-header__title uppercase m-0'>Deals</h2>
+                    </div>
+                    <div className='card-content'>
+                        <div className='grid datatable-controls'>
+                            <div className='col-6'>
+                                <div className='contact-top-controls'>
+                                    <Button
+                                        className='contact-top-controls__button m-r-20px'
+                                        icon='pi pi-plus-circle'
+                                        severity='success'
+                                        type='button'
+                                    />
+                                    <Button
+                                        severity='success'
+                                        type='button'
+                                        icon='pi pi-print'
+                                        onClick={printTableData}
+                                    />
+                                </div>
+                            </div>
+                            <div className='col-6 text-right'>
+                                <Button
+                                    className='contact-top-controls__button m-r-20px'
+                                    label='Advanced search'
+                                    severity='success'
+                                    type='button'
+                                />
+                                <span className='p-input-icon-right'>
+                                    <i className='pi pi-search' />
+                                    <InputText
+                                        value={globalSearch}
+                                        onChange={(e) => setGlobalSearch(e.target.value)}
+                                    />
+                                </span>
+                            </div>
+                        </div>
+                        <div className='grid'>
+                            <div className='col-12'>
+                                <DataTable
+                                    value={deals}
+                                    lazy
+                                    paginator
+                                    first={lazyState.first}
+                                    rows={lazyState.rows}
+                                    rowsPerPageOptions={ROWS_PER_PAGE}
+                                    totalRecords={totalRecords}
+                                    onPage={pageChanged}
+                                    onSort={sortData}
+                                    sortOrder={lazyState.sortOrder}
+                                    sortField={lazyState.sortField}
+                                >
+                                    <Column field='accountuid' header='Account'></Column>
+                                    <Column field='contactinfo' header='Customer'></Column>
+                                    <Column field='dealtype' header='Type'></Column>
+                                    <Column field='created' header='Date'></Column>
+                                    <Column field='inventoryinfo' header='Info (Vehicle)'></Column>
+                                </DataTable>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
