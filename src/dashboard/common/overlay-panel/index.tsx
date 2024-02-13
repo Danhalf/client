7c86e6495bb0c0305c92223654289181d2889c2d
Danhalf <danhel@rocketmail.com>
2024-02-13 09:42:49 +0200

    changed: button to icon

--- src/dashboard/common/overlay-panel/index.tsx
+++ src/dashboard/common/overlay-panel/index.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 
 import { useRef } from "react";
 import { OverlayPanel, OverlayPanelProps } from "primereact/overlaypanel";
-import { Button } from "primereact/button";
 
 interface InfoOverlayPanelProps extends OverlayPanelProps {
     panelTitle?: string;
@@ -12,15 +11,12 @@ export const InfoOverlayPanel = ({ panelTitle, ...props }: InfoOverlayPanelProps
     const op = useRef<OverlayPanel>(null);
     return (
         <div className='info-panel'>
-            <Button
-                type='button'
+            <i
                 onClick={(e) => {
                     op.current?.toggle(e);
                 }}
-                className='p-button info-panel__button'
-            >
-                <i className='custom-target-icon icon adms-help p-text-secondary p-overlay-badge info-panel__icon' />
-            </Button>
+                className='icon adms-help p-text-secondary p-overlay-badge info-panel__icon'
+            />
             <OverlayPanel ref={op} dismissable={false} className='info-panel__panel shadow-3'>
                 <div className='info-panel__title'>{panelTitle}</div>
                 <div className='info-panel__body'>{props.children}</div>
