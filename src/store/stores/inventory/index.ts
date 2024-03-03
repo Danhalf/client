29dcf68d780c693bcbeb164461fd945da5b8b17c
danhalf <danhel@rocketmail.com>
2024-03-03 15:14:51 +0200

    remove: audit

--- src/store/stores/inventory/index.ts
+++ src/store/stores/inventory/index.ts
@@ -179,7 +179,7 @@ export class InventoryStore {
 
     public changeInventory = action(
         ({ key, value }: { key: keyof Inventory; value: string | number }) => {
-            if (this._inventory && key !== "extdata" && key !== "options_info" && key !== "Audit") {
+            if (this._inventory && key !== "extdata" && key !== "options_info") {
                 (this._inventory as Record<typeof key, string | number>)[key] = value;
             }
         }
