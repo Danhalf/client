3777a816dbd270741160408dd0f1c4a1cf35f81e
danhalf <danhel@rocketmail.com>
2024-03-15 19:37:48 +0200

    removed: app create instance

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { Outlet } from "react-router-dom";
 import { store } from "store";
 import { StoreContext } from "store/context";
-import { useNavigate } from "react-router-dom";
-import { createApiDashboardInstance } from "http/index";
 
 export default function App() {
-    const navigate = useNavigate();
-
-    createApiDashboardInstance(navigate);
     return (
         <StoreContext.Provider value={store}>
             <Outlet />
