0ee9788893aa69805227ef9dccb6970a78dd2697
danhalf <danhel@rocketmail.com>
2024-04-12 20:53:38 +0300

    added: accordion list filled

--- src/dashboard/deals/form/retail/index.tsx
+++ src/dashboard/deals/form/retail/index.tsx
@@ -13,13 +13,29 @@ const DealRetailTradeSecond = lazy(() =>
 const DealRetailTag = lazy(() =>
     import("./tag").then((module) => ({ default: module.DealRetailTag }))
 );
+const DealRetailInsurance = lazy(() =>
+    import("./insurance").then((module) => ({ default: module.DealRetailInsurance }))
+);
+const DealRetailProducts = lazy(() =>
+    import("./products").then((module) => ({ default: module.DealRetailProducts }))
+);
+const DealRetailFinances = lazy(() =>
+    import("./finances").then((module) => ({ default: module.DealRetailFinances }))
+);
+const DealRetailPickup = lazy(() =>
+    import("./pickup").then((module) => ({ default: module.DealRetailPickup }))
+);
 
 export const DealRetail: Pick<Inventory, "label" | "items"> = {
-    label: "General information",
+    label: "Retail (Cash)",
     items: [
         { itemLabel: "Liens", component: <DealRetailLiens /> },
         { itemLabel: "Trade 1", component: <DealRetailTradeFirst /> },
         { itemLabel: "Trade 2", component: <DealRetailTradeSecond /> },
         { itemLabel: "Tag", component: <DealRetailTag /> },
+        { itemLabel: "Insurance", component: <DealRetailInsurance /> },
+        { itemLabel: "Products", component: <DealRetailProducts /> },
+        { itemLabel: "Finances", component: <DealRetailFinances /> },
+        { itemLabel: "Pickup", component: <DealRetailPickup /> },
     ],
 };
