d02fb7e6f1029a4a9f4842cedb4820c62b783268
danhalf <danhel@rocketmail.com>
2024-04-16 19:01:16 +0300

    added: pickup

--- src/dashboard/deals/form/retail/contract/index.tsx
+++ src/dashboard/deals/form/retail/contract/index.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react-lite";
 import { ReactElement } from "react";
 import "./index.css";
 import { InputText } from "primereact/inputtext";
-import { CurrencyInput, DateInput } from "dashboard/common/form/inputs";
+import { CurrencyInput, DateInput, PercentInput } from "dashboard/common/form/inputs";
 import { Dropdown } from "primereact/dropdown";
 
 export const DealRetailContract = observer((): ReactElement => {
@@ -10,7 +10,7 @@ export const DealRetailContract = observer((): ReactElement => {
         <div className='grid deal-retail-contract row-gap-2'>
             <div className='col-3'>
                 <span className='p-float-label'>
-                    <InputText className='deal-insurance__text-input w-full' />
+                    <InputText className='deal-contract__text-input w-full' />
                     <label className='float-label'>Account Number</label>
                 </span>
             </div>
@@ -32,7 +32,7 @@ export const DealRetailContract = observer((): ReactElement => {
                     <label className='float-label'>Payment Frequency</label>
                 </span>
             </div>
-            <div className='col-6'>
+            <div className='col-3'>
                 <span className='p-float-label'>
                     <Dropdown
                         optionLabel='name'
@@ -45,6 +45,9 @@ export const DealRetailContract = observer((): ReactElement => {
                 </span>
             </div>
             <div className='col-3'>
+                <PercentInput labelPosition='top' value={0} title='Interest Rate' />
+            </div>
+            <div className='col-3'>
                 <CurrencyInput labelPosition='top' title='Payment Amount' />
             </div>
             <div className='col-3'>
@@ -54,53 +57,39 @@ export const DealRetailContract = observer((): ReactElement => {
             <hr className='form-line' />
 
             <div className='col-3'>
-                <span className='p-float-label'>
-                    <InputText className='deal-insurance__text-input w-full' />
-                    <label className='float-label'>Insurance Co#</label>
-                </span>
-            </div>
-
-            <div className='col-3'>
-                <DateInput name='Effective Date' />
+                <CurrencyInput labelPosition='top' title='Total Interest' />
             </div>
             <div className='col-3'>
-                <DateInput name='Expiration Date' />
-            </div>
-
-            <div className='col-6'>
-                <span className='p-float-label'>
-                    <InputText className='deal-insurance__text-input w-full' />
-                    <label className='float-label'>Agent's Address</label>
-                </span>
+                <CurrencyInput labelPosition='top' title='Total of Payments' />
             </div>
             <div className='col-3'>
                 <span className='p-float-label'>
-                    <InputText className='deal-insurance__text-input w-full' />
-                    <label className='float-label'>Phone Number</label>
+                    <InputText className='deal-contract__text-input w-full' />
+                    <label className='float-label'>Days to First Payment</label>
                 </span>
+            </div>
+            <div className='col-3'>
+                <DateInput name='First Payment Due' />
+            </div>
+            <div className='col-3'>
+                <DateInput name='Final Payment Due' />
             </div>
 
             <hr className='form-line' />
 
             <div className='col-3'>
-                <CurrencyInput labelPosition='top' title='Coverage Collision' />
+                <PercentInput labelPosition='top' value={0} title='Late Fee' />
             </div>
-            <div className='col-2'>
-                <span className='p-float-label'>
-                    <InputText className='deal-insurance__text-input w-full' />
-                    <label className='float-label'>Liability Row</label>
-                </span>
+            <div className='col-3'>
+                <CurrencyInput labelPosition='top' title='Flat/ Min Late Fee' />
             </div>
-            <div className='col-2'>
-                <span className='p-float-label'>
-                    <InputText className='deal-insurance__text-input w-full' />
-                    <label className='float-label'>Liability Row</label>
-                </span>
+            <div className='col-3'>
+                <CurrencyInput labelPosition='top' title='Late Fee Cap' />
             </div>
-            <div className='col-2'>
+            <div className='col-3'>
                 <span className='p-float-label'>
-                    <InputText className='deal-insurance__text-input w-full' />
-                    <label className='float-label'>Liability Row</label>
+                    <InputText className='deal-contract__text-input w-full' />
+                    <label className='float-label'>Grace Period</label>
                 </span>
             </div>
         </div>
