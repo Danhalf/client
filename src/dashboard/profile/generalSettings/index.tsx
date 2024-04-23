e8b3e66cfb6878be1aa05c3fafe92316d9382664
danhalf <danhel@rocketmail.com>
2024-04-23 19:52:16 +0300

    added: general settings styles

--- src/dashboard/profile/generalSettings/index.tsx
+++ src/dashboard/profile/generalSettings/index.tsx
@@ -9,6 +9,8 @@ import { SettingsAccount } from "./account";
 import { SettingsContract } from "./contract";
 import { SettingsLease } from "./lease";
 import { ReactElement } from "react";
+import { Button } from "primereact/button";
+import { useNavigate } from "react-router-dom";
 
 interface TabItem {
     settingName: string;
@@ -16,6 +18,7 @@ interface TabItem {
 }
 
 export const GeneralSettings = (): ReactElement => {
+    const navigate = useNavigate();
     const tabItems: TabItem[] = [
         {
             settingName: "Deals",
@@ -58,17 +61,31 @@ export const GeneralSettings = (): ReactElement => {
     ];
 
     return (
-        <TabView className='general-settings__tabs'>
-            {tabItems.map(({ settingName, component }) => {
-                return (
-                    <TabPanel
-                        header={settingName}
-                        children={component}
-                        key={settingName}
-                        className='general-settings__panel'
-                    />
-                );
-            })}
-        </TabView>
+        <div className='grid relative'>
+            <Button
+                icon='pi pi-times'
+                className='p-button close-button'
+                onClick={() => navigate("/dashboard")}
+            />
+            <div className='col-12'>
+                <div className='card'>
+                    <div className='card-header flex'>
+                        <h2 className='card-header__title uppercase m-0'>General settings</h2>
+                    </div>
+                    <TabView className='general-settings__tabs'>
+                        {tabItems.map(({ settingName, component }) => {
+                            return (
+                                <TabPanel
+                                    header={settingName}
+                                    children={component}
+                                    key={settingName}
+                                    className='general-settings__panel'
+                                />
+                            );
+                        })}
+                    </TabView>
+                </div>
+            </div>
+        </div>
     );
 };
