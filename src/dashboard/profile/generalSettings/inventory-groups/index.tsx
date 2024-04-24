401d650ed0be2e8785c3dda9594460b8eb6759a5
danhalf <danhel@rocketmail.com>
2024-04-24 20:08:19 +0300

    changed: styles inventory groups table

--- src/dashboard/profile/generalSettings/inventory-groups/index.tsx
+++ src/dashboard/profile/generalSettings/inventory-groups/index.tsx
@@ -1,35 +1,74 @@
 import { Column, ColumnProps } from "primereact/column";
 import "./index.css";
 import { DataTable } from "primereact/datatable";
+import { Button } from "primereact/button";
+import { Checkbox } from "primereact/checkbox";
+import { useState } from "react";
+
+const renderColumnsData: Pick<ColumnProps, "header" | "field">[] = [
+    { field: "group", header: "Group" },
+];
+
+interface SettingsInventoryData {
+    id: number;
+    group: string;
+}
+const inventoryData: SettingsInventoryData[] = [
+    { id: 1, group: "Cars" },
+    { id: 2, group: "The Internet" },
+    { id: 3, group: "Local advertisement" },
+    { id: 4, group: "Direct mail" },
+];
 
 export const SettingsInventoryGroups = () => {
-    const renderColumnsData: Pick<ColumnProps, "header" | "field">[] = [
-        { field: "", header: "" },
-        { field: "group", header: "Group" },
-        { field: "action", header: "Action" },
-    ];
-
-    const renderRowsData = [
-        { id: 1, group: "Cars", action: "Action 1" },
-        { id: 2, group: "The Internet", action: "Action 2" },
-        { id: 3, group: "Local advertisement", action: "Action 3" },
-        { id: 3, group: "Direct mail", action: "Action 3" },
-    ];
+    const [selectedInventory, setSelectedInventory] = useState<SettingsInventoryData[] | null>(
+        null
+    );
 
     return (
         <div className='settings-form'>
             <div className='settings-form__title'>Inventory groups</div>
             <div className='grid settings-inventory'>
                 <div className='col-12'>
-                    <DataTable showGridlines value={renderRowsData} rowHover resizableColumns>
+                    <DataTable
+                        value={inventoryData}
+                        selectionMode={"multiple"}
+                        selection={selectedInventory!}
+                        onSelectionChange={(e) => setSelectedInventory(e.value)}
+                    >
+                        <Column selectionMode='multiple' headerStyle={{ width: "3rem" }}></Column>
                         {renderColumnsData.map(({ field, header }) => (
                             <Column
                                 field={field}
+                                bodyStyle={{ height: "30px" }}
                                 header={header}
                                 key={field}
-                                headerClassName='cursor-move'
                             />
                         ))}
+                        <Column
+                            headerStyle={{ width: "6rem" }}
+                            header={"Actions"}
+                            body={(rowData) => {
+                                return (
+                                    <div className='flex gap-3'>
+                                        <Button
+                                            className='p-button p-button-outlined'
+                                            severity='secondary'
+                                            onClick={() => {}}
+                                        >
+                                            Edit
+                                        </Button>
+                                        <Button
+                                            className='p-button p-button-outlined'
+                                            severity='secondary'
+                                            onClick={() => {}}
+                                        >
+                                            Delete
+                                        </Button>
+                                    </div>
+                                );
+                            }}
+                        />
                     </DataTable>
                 </div>
             </div>
