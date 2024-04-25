769d33d24db2319f55c845a5e5007009506cc936
danhalf <danhel@rocketmail.com>
2024-04-25 12:01:19 +0300

    added: edit row form

--- src/dashboard/profile/generalSettings/inventory-groups/index.tsx
+++ src/dashboard/profile/generalSettings/inventory-groups/index.tsx
@@ -1,8 +1,9 @@
-import { Column, ColumnProps } from "primereact/column";
+import { Column, ColumnEditorOptions, ColumnProps } from "primereact/column";
 import "./index.css";
-import { DataTable } from "primereact/datatable";
+import { DataTable, DataTableRowEditCompleteEvent } from "primereact/datatable";
 import { Button } from "primereact/button";
 import { useState } from "react";
+import { InputText } from "primereact/inputtext";
 
 const renderColumnsData: Pick<ColumnProps, "header" | "field">[] = [
     { field: "group", header: "Group" },
@@ -20,9 +21,50 @@ const inventoryData: SettingsInventoryData[] = [
 ];
 
 export const SettingsInventoryGroups = () => {
+    const [inventorySettings, setInventorySettings] =
+        useState<SettingsInventoryData[]>(inventoryData);
     const [selectedInventory, setSelectedInventory] = useState<SettingsInventoryData[] | null>(
         null
     );
+
+    const handleEdit = (e: any, o: any) => {
+        // eslint-disable-next-line no-console
+        // console.log(dataTableRef.current.getElement());
+    };
+
+    const handleDelete = (e: any, o: any) => {
+        if (selectedInventory) {
+            // eslint-disable-next-line no-console
+            console.log(e);
+        }
+    };
+
+    const onRowEditComplete = (e: DataTableRowEditCompleteEvent) => {
+        let editedData = [...inventorySettings];
+        let { newData, index } = e;
+
+        editedData[index] = newData as SettingsInventoryData;
+
+        setInventorySettings(editedData);
+    };
+
+    const textEditor = (options: ColumnEditorOptions) => {
+        return (
+            <div className='flex row-edit'>
+                <InputText
+                    type='text'
+                    value={options.value}
+                    className='row-edit__input'
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        options.editorCallback!(e.target.value)
+                    }
+                />
+                <Button className='p-button row-edit__button' onClick={() => {}}>
+                    Save
+                </Button>
+            </div>
+        );
+    };
 
     return (
         <div className='settings-form'>
@@ -35,9 +77,11 @@ export const SettingsInventoryGroups = () => {
             <div className='grid settings-inventory'>
                 <div className='col-12'>
                     <DataTable
-                        value={inventoryData}
-                        selectionMode={"multiple"}
+                        value={inventorySettings}
+                        selectionMode={"checkbox"}
                         selection={selectedInventory!}
+                        onRowEditComplete={onRowEditComplete}
+                        editMode='row'
                         onSelectionChange={(e) => setSelectedInventory(e.value)}
                     >
                         <Column selectionMode='multiple' headerStyle={{ width: "3rem" }}></Column>
@@ -47,18 +91,25 @@ export const SettingsInventoryGroups = () => {
                                 bodyStyle={{ height: "30px" }}
                                 header={header}
                                 key={field}
+                                editor={(options) => textEditor(options)}
                             />
                         ))}
                         <Column
                             headerStyle={{ width: "6rem" }}
                             header={"Actions"}
-                            body={(rowData) => {
+                            rowEditor
+                            body={(rowData, options, ...rest) => {
                                 return (
                                     <div className='flex gap-3'>
                                         <Button
                                             className='p-button p-button-outlined'
-                                            severity='secondary'
-                                            onClick={() => {}}
+                                            severity={
+                                                options.rowEditor?.editing ? "secondary" : "success"
+                                            }
+                                            onClick={(event) => {
+                                                options.rowEditor?.onInitClick!(event);
+                                            }}
+                                            disabled={options.rowEditor?.editing}
                                         >
                                             Edit
                                         </Button>
