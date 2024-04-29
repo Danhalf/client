d82fefc7f3a279acd26b3eb83a1fd347673732de
danhalf <danhel@rocketmail.com>
2024-04-29 08:12:48 +0300

    added: dropdown fields

--- src/dashboard/common/dialog/search/index.tsx
+++ src/dashboard/common/dialog/search/index.tsx
@@ -1,11 +1,23 @@
-import { ReactElement } from "react";
+import { ReactElement, useCallback, useEffect, useState } from "react";
 import { DashboardDialog, DashboardDialogProps } from "..";
 import { InputText } from "primereact/inputtext";
 import "./index.css";
+import { Dropdown, DropdownProps } from "primereact/dropdown";
+import { AuthUser } from "http/services/auth.service";
+import { LS_APP_USER } from "common/constants/localStorage";
+import {
+    ListData,
+    MakesListData,
+    getInventoryAutomakesList,
+    getInventoryExteriorColorsList,
+    getInventoryInteriorColorsList,
+} from "http/services/inventory-service";
+import { getKeyValue } from "services/local-storage.service";
 
 export interface SearchField<T> {
     key: keyof T & string;
     value: string | undefined;
+    type: "text" | "dropdown";
 }
 interface AdvancedSearchDialogProps<T> extends DashboardDialogProps {
     onInputChange: (key: keyof T, value: string) => void;
@@ -21,6 +33,66 @@ export const AdvancedSearchDialog = <T,>({
     fields,
     action,
 }: AdvancedSearchDialogProps<T>): ReactElement => {
+    const [automakesList, setAutomakesList] = useState<MakesListData[]>([]);
+    const [automakesModelList, setAutomakesModelList] = useState<ListData[]>([]);
+
+    useEffect(() => {
+        // const authUser: AuthUser = getKeyValue(LS_APP_USER);
+        getInventoryAutomakesList().then((list) => {
+            if (list) {
+                const upperCasedList = list.map((item) => ({
+                    ...item,
+                    name: item.name.toUpperCase(),
+                }));
+                setAutomakesList(upperCasedList);
+            }
+        });
+    }, []);
+
+    // const handleSelectMake = useCallback(() => {
+    //     getAutoMakeModelList(makeSting).then((list) => {
+    //         if (list && Object.keys(list).length) {
+    //             setAutomakesModelList(list);
+    //         } else {
+    //             setAutomakesModelList([]);
+    //         }
+    //     });
+    // }, [inventory.Make]);
+
+    // useEffect(() => {
+    //     if (inventory.Make) handleSelectMake();
+    // }, [handleSelectMake, inventory.Make]);
+
+    const selectedAutoMakesTemplate = (option: MakesListData, props: DropdownProps) => {
+        if (option) {
+            return (
+                <div className='flex align-items-center'>
+                    <img
+                        alt={option.name}
+                        // src={option?.logo || defaultMakesLogo}
+                        className='mr-2 vehicle-general__dropdown-icon'
+                    />
+                    <div>{option.name}</div>
+                </div>
+            );
+        }
+
+        return <span>{props.placeholder}</span>;
+    };
+
+    const autoMakesOptionTemplate = (option: MakesListData) => {
+        return (
+            <div className='flex align-items-center'>
+                <img
+                    alt={option.name}
+                    // src={option?.logo || defaultMakesLogo}
+                    className='mr-2 vehicle-general__dropdown-icon'
+                />
+                <div>{option.name}</div>
+            </div>
+        );
+    };
+
     return (
         <DashboardDialog
             className='search-dialog'
@@ -33,13 +105,22 @@ export const AdvancedSearchDialog = <T,>({
         >
             <div className='flex flex-column gap-4 pt-4'>
                 {fields &&
-                    fields.map(({ key, value }) => (
+                    fields.map(({ key, value, type }) => (
                         <span className='p-float-label p-input-icon-right' key={key}>
-                            <InputText
-                                className='w-full'
-                                value={value ?? ""}
-                                onChange={({ target }) => onInputChange(key, target.value)}
-                            />
+                            {type === "text" && (
+                                <InputText
+                                    className='w-full'
+                                    value={value ?? ""}
+                                    onChange={({ target }) => onInputChange(key, target.value)}
+                                />
+                            )}
+                            {type === "dropdown" && (
+                                <Dropdown
+                                    className='w-full'
+                                    value={value ?? ""}
+                                    onChange={({ target }) => onInputChange(key, target.value)}
+                                />
+                            )}
                             {value && onSearchClear && (
                                 <i
                                     className='pi pi-times cursor-pointer search-dialog__clear'
