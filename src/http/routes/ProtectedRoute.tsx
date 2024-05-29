cf5c00ca171d64b219c6fd792e2ea748e342cf44
Danhalf <danhel@rocketmail.com>
2024-05-29 16:06:49 +0300

    added: permissions API

--- src/http/routes/ProtectedRoute.tsx
+++ src/http/routes/ProtectedRoute.tsx
@@ -1,8 +1,9 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { ReactElement, ReactNode, useEffect, useState } from "react";
 import { LS_APP_USER } from "common/constants/localStorage";
 import { AuthUser } from "http/services/auth.service";
-import { getKeyValue } from "services/local-storage.service";
+import { getKeyValue, localStorageClear, setKey } from "services/local-storage.service";
+import { getUserPermissions } from "http/services/auth-user.service";
 
 interface UserRoles {
     admin: boolean;
@@ -16,20 +17,60 @@ interface ProtectedRouteProps {
     children?: ReactNode;
 }
 
+const throttle = (func: (...args: any[]) => void, limit: number) => {
+    let inThrottle: boolean;
+    return function (this: any, ...args: any[]) {
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => (inThrottle = false), limit);
+        }
+    };
+};
+
 export const useAuth = (): AuthUser | null => {
     const [authUser, setAuthUser] = useState<AuthUser | null>(() => getKeyValue(LS_APP_USER));
+    const location = useLocation();
+
+    const updatePermissions = async () => {
+        if (authUser) {
+            getUserPermissions(authUser.useruid).then(
+                ({ uaSystemAdmin, uaLocationAdmin, uaManager, uaSalesPerson }) => {
+                    const user: AuthUser = getKeyValue(LS_APP_USER);
+
+                    user.islocaladmin = uaLocationAdmin || 0;
+                    authUser.islocaladmin = uaLocationAdmin || 0;
+                    user.isadmin = uaSystemAdmin || 0;
+                    authUser.isadmin = uaSystemAdmin || 0;
+                    user.ismanager = uaManager || 0;
+                    authUser.ismanager = uaManager || 0;
+                    user.issalesperson = uaSalesPerson || 0;
+                    authUser.issalesperson = uaSalesPerson || 0;
+
+                    localStorageClear(LS_APP_USER);
+                    setKey(LS_APP_USER, JSON.stringify(user));
+                    setAuthUser(user);
+                }
+            );
+        }
+    };
+
+    const throttledUpdatePermissions = throttle(updatePermissions, 3000);
 
     useEffect(() => {
         const handleStorageChange = () => {
             setAuthUser(getKeyValue(LS_APP_USER));
         };
 
+        throttledUpdatePermissions();
         window.addEventListener("storage", handleStorageChange);
 
         return () => {
             window.removeEventListener("storage", handleStorageChange);
         };
-    }, []);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location]);
 
     return authUser;
 };
@@ -48,14 +89,10 @@ const ProtectedRoute = ({ notAllowed, children }: ProtectedRouteProps): ReactEle
             };
 
             if (notAllowed) {
-                setHasRequiredRole(
-                    notAllowed.some((role) => {
-                        return !userRoles[role];
-                    })
-                );
+                setHasRequiredRole(notAllowed.some((role) => !userRoles[role]));
             }
         }
-    }, [notAllowed, authUser]);
+    }, [authUser, notAllowed]);
 
     if (!authUser) {
         return <Navigate to='/' replace />;
