0d6eef37367e8a1984251cd66e69e62186bd9954
danhalf <danhel@rocketmail.com>
2024-07-02 19:10:13 +0300

    added: create custom report page

---
+++ src/dashboard/reports/form/index.tsx
@@ -0,0 +1,78 @@
+import { ReportCollection } from "common/models/reports";
+import { getUserReportCollectionsContent } from "http/services/reports.service";
+import { Accordion, AccordionTab } from "primereact/accordion";
+import { Button } from "primereact/button";
+import { ReactElement, Suspense, useEffect, useState } from "react";
+import { useStore } from "store/hooks";
+import "./index.css";
+
+export const ReportForm = (): ReactElement => {
+    const userStore = useStore().userStore;
+    const { authUser } = userStore;
+    const [collections, setCollections] = useState<ReportCollection[]>([]);
+
+    const handleGetUserReportCollections = (useruid: string) =>
+        getUserReportCollectionsContent(useruid).then((response) => {
+            if (Array.isArray(response)) {
+                setCollections(response);
+            } else {
+                setCollections([]);
+            }
+        });
+
+    useEffect(() => {
+        if (authUser) {
+            handleGetUserReportCollections(authUser.useruid);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [authUser]);
+
+    return (
+        <Suspense>
+            <div className='grid relative'>
+                <div className='col-12'>
+                    <div className='card report'>
+                        <div className='card-header flex'>
+                            <h2 className='report__title uppercase m-0'>Create custom report</h2>
+                        </div>
+                        <div className='card-content report__card grid'>
+                            <div className='col-4'>
+                                <Accordion multiple className='report__accordion'>
+                                    {collections &&
+                                        collections.map(
+                                            ({ itemUID, name, documents }: ReportCollection) => (
+                                                <AccordionTab
+                                                    key={itemUID}
+                                                    header={name}
+                                                    className='report__accordion-tab'
+                                                >
+                                                    {documents &&
+                                                        documents.map((report) => (
+                                                            <div
+                                                                className='report__list-item'
+                                                                key={report.itemUID}
+                                                            >
+                                                                <p>{report.name}</p>
+                                                            </div>
+                                                        ))}
+                                                </AccordionTab>
+                                            )
+                                        )}
+                                </Accordion>
+                            </div>
+                        </div>
+                        <div className='flex justify-content-end gap-3 mt-8 mr-3'>
+                            <Button className='uppercase px-6 report__button' outlined>
+                                Cancel
+                            </Button>
+                            <Button className='uppercase px-6 report__button' outlined>
+                                Create
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Suspense>
+    );
+};
+export default ReportForm;
