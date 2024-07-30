da875c891f3fb707f5ce571bcc1cc83f68f170ba
danhalf <danhel@rocketmail.com>
2024-07-30 21:52:35 +0300

    added: quick pay form

--- src/dashboard/accounts/take-payment-form/balance-adjustment/index.tsx
+++ src/dashboard/accounts/take-payment-form/balance-adjustment/index.tsx
@@ -1,10 +1,6 @@
 import { ReactElement } from "react";
-// import { useParams } from "react-router-dom";
-
 import { observer } from "mobx-react-lite";
 
 export const AccountBalanceAdjustment = observer((): ReactElement => {
-    // const { id } = useParams();
-
     return <div className='quick-pay'>Balance</div>;
 });
