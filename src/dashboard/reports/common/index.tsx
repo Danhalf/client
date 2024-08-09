a00ba3d7007a89dc6a8c0ec2b0d4179f828c3dca
danhalf <danhel@rocketmail.com>
2024-08-09 18:27:37 +0300

    added: dialog mock data

--- src/dashboard/reports/common/index.tsx
+++ src/dashboard/reports/common/index.tsx
@@ -2,11 +2,26 @@ import { ReportACL } from "common/models/reports";
 import { DashboardDialog } from "dashboard/common/dialog";
 import { getReportAccessList } from "http/services/reports.service";
 import { Button, ButtonProps } from "primereact/button";
+import { Checkbox } from "primereact/checkbox";
+import { Column, ColumnProps } from "primereact/column";
 import { DataTable } from "primereact/datatable";
 import { InputText } from "primereact/inputtext";
+import { MultiSelect } from "primereact/multiselect";
 import { PanelHeaderTemplateOptions } from "primereact/panel";
 import { ReactElement, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+interface TableColumnProps extends ColumnProps {
+    field: keyof ReportACL | "role" | "access";
+}
+
+export type TableColumnsList = Pick<TableColumnProps, "header" | "field">;
+
+const renderColumnsData: TableColumnsList[] = [
+    { field: "username", header: "User name" },
+    { field: "role", header: "Role" },
+    { field: "access", header: "Access" },
+];
 
 interface EditAccessDialogProps {
     visible: boolean;
@@ -14,8 +29,48 @@ interface EditAccessDialogProps {
     reportuid: string;
 }
 
+const filterOptions = [
+    {
+        label: "Role",
+        items: [
+            { name: "All roles", value: "All roles" },
+            { name: "Admin", value: "Admin" },
+            { name: "Manager", value: "Manager" },
+            { name: "Sales_person", value: "Sales person" },
+        ],
+    },
+    {
+        label: "Access",
+        items: [
+            { name: "All access", value: "All access" },
+            { name: "Denied", value: "Denied" },
+            { name: "Granted", value: "Granted" },
+        ],
+    },
+];
+
+const mockAccessList: any[] = [
+    {
+        username: "John Doe",
+        role: "Manager",
+        access: 0,
+    },
+    {
+        username: "John Smith",
+        role: "Admin",
+        access: 1,
+    },
+    {
+        username: "Mary Smith",
+        role: "Manager",
+        access: 1,
+    },
+];
+
 const EditAccessDialog = ({ visible, onHide, reportuid }: EditAccessDialogProps): ReactElement => {
-    const [accessList, setAccessList] = useState<ReportACL[]>([]);
+    const [accessList, setAccessList] = useState<ReportACL[]>(mockAccessList);
+    const [selectedRole, setSelectedRole] = useState();
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
     useEffect(() => {
         if (visible) {
@@ -27,6 +82,34 @@ const EditAccessDialog = ({ visible, onHide, reportuid }: EditAccessDialogProps)
         }
     }, [visible, reportuid]);
 
+    const accessField = (data: ReportACL & { access: number }): ReactElement => {
+        const accessBlock = (
+            <label
+                className={`access-field ${
+                    data.access === 1 ? "access-field--green row--selected" : ""
+                }`}
+            >
+                <Checkbox
+                    className='access-field__checkbox'
+                    onClick={() => {
+                        const newList = accessList.map((item: any) => {
+                            if (item.username === data.username) {
+                                setIsButtonDisabled(false);
+                                return { ...item, access: item.access === 1 ? 0 : 1 };
+                            }
+                            return item;
+                        });
+
+                        setAccessList(newList);
+                    }}
+                    checked={data.access === 1}
+                />
+                {data.access === 1 ? "Granted" : "Denied"}
+            </label>
+        );
+        return accessBlock;
+    };
+
     return (
         <DashboardDialog
             className='edit-access'
@@ -34,8 +117,51 @@ const EditAccessDialog = ({ visible, onHide, reportuid }: EditAccessDialogProps)
             header='Edit Access'
             visible={visible}
             onHide={onHide}
+            buttonDisabled={isButtonDisabled}
         >
-            <DataTable showGridlines value={accessList}></DataTable>
+            <div className='grid'>
+                <div className='col-5'>
+                    <MultiSelect
+                        optionLabel='name'
+                        options={filterOptions}
+                        className='w-full'
+                        optionGroupChildren='items'
+                        optionGroupLabel='label'
+                        showSelectAll={false}
+                        value={selectedRole}
+                        display='chip'
+                        panelHeaderTemplate={<></>}
+                        onChange={(e) => {
+                            e.stopPropagation();
+                            setSelectedRole(e.value);
+                        }}
+                    />
+                </div>
+                <div className='col-4 ml-auto'>
+                    <span className='p-input-icon-right'>
+                        <i className='pi pi-search' />
+                        <InputText placeholder='Search' className='w-full' />
+                    </span>
+                </div>
+                <div className='col-12'>
+                    <DataTable showGridlines value={accessList} emptyMessage='User not found.'>
+                        {renderColumnsData.map((column) => (
+                            <Column
+                                key={column.field}
+                                field={column.field}
+                                header={column.header}
+                                body={(data) => {
+                                    if (column.field === "access") {
+                                        return accessField(data as ReportACL & { access: number });
+                                    } else {
+                                        return data[column.field];
+                                    }
+                                }}
+                            />
+                        ))}
+                    </DataTable>
+                </div>
+            </div>
         </DashboardDialog>
     );
 };
@@ -65,7 +191,7 @@ export const ActionButtons = ({ reportuid }: { reportuid: string }): ReactElemen
                 <Button
                     className='p-button reports-actions__button'
                     icon='icon adms-password'
-                    outlined
+                    outlined={!editAccessActive}
                     tooltip='Edit Access'
                     onClick={handleEditAccess}
                 />
