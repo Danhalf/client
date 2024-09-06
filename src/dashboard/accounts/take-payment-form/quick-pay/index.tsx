47aa7af423067e969e7c1c0fdee099866c5a9e16
Danhalf <danhel@rocketmail.com>
2024-09-06 12:08:08 +0300

    added: payment distribution API

--- src/dashboard/accounts/take-payment-form/quick-pay/index.tsx
+++ src/dashboard/accounts/take-payment-form/quick-pay/index.tsx
@@ -22,7 +22,7 @@ const currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear
 export const AccountQuickPay = observer((): ReactElement => {
     const store = useStore().accountStore;
     const {
-        accountPaymentsInfo: { QuickPay },
+        accountPaymentsInfo: { QuickPay, PaymentDistribution },
     } = store;
     return (
         <div className='quick-pay'>
@@ -162,70 +162,98 @@ export const AccountQuickPay = observer((): ReactElement => {
                         <label className='take-payment__label color-dusty-blue'>
                             Down/ Pickup Pmt:
                         </label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.DownPickupPayment || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item color-red'>
                         <label className='take-payment__label'>Fees:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.Fees || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item color-green'>
                         <label className='take-payment__label'>Principal:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.Principal || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item color-green'>
                         <label className='take-payment__label'>Additional Principal:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.AdditionalPrincipal || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item color-green'>
                         <label className='take-payment__label'>Interest:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.Interest || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item color-purple'>
                         <label className='take-payment__label'>Taxes Paid:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.TaxesPaid || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item take-payment__item--bold'>
                         <label className='take-payment__label color-deep-blue'>Total Paid:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.TotalPaid || "0.00"}
+                        </span>
                     </div>
 
                     <hr className='form-line' />
 
                     <div className='take-payment__item'>
                         <label className='take-payment__label'>Next Pmt Due:</label>
-                        <span className='take-payment__value'>07/15/2024</span>
+                        <span className='take-payment__value'>
+                            {PaymentDistribution?.NextPmtDue}
+                        </span>
                     </div>
                     <div className='take-payment__item'>
                         <label className='take-payment__label'>Remaining Past Due:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            ${PaymentDistribution?.RemainingPastDue || "0.00"}
+                        </span>
                     </div>
 
                     <hr className='form-line' />
 
                     <div className='take-payment__item take-payment__item--bold'>
                         <label className='take-payment__label color-green'>New Acct Balance:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.NewAccountBalance || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item take-payment__item--bold'>
                         <label className='take-payment__label color-violet'>
                             New Down Pmt Balance:
                         </label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.NewDownPaymentBalance || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item'>
                         <label className='take-payment__label color-red'>New Fees Added:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.NewFeesAdded || "0.00"}
+                        </span>
                     </div>
                     <div className='take-payment__item color-red take-payment__item--bold'>
                         <label className='take-payment__label'>New Fees Balance:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.NewFeesBalance || "0.00"}
+                        </span>
                     </div>
 
                     <hr className='form-line' />
 
                     <div className='take-payment__item take-payment__item--bold take-payment__item--subtitle'>
                         <label className='take-payment__label'>New Total Balance:</label>
-                        <span className='take-payment__value'>$ 0.00</span>
+                        <span className='take-payment__value'>
+                            $ {PaymentDistribution?.NewTotalBalance || "0.00"}
+                        </span>
                     </div>
                 </div>
             </div>
