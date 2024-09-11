db0ea012f45a85341f3e65f8a5f74abab6311593
Danhalf <danhel@rocketmail.com>
2024-09-11 17:18:11 +0300

    added: contact access

--- src/dashboard/contacts/form/index.tsx
+++ src/dashboard/contacts/form/index.tsx
@@ -16,6 +16,8 @@ import { Form, Formik, FormikProps } from "formik";
 import { Contact, ContactExtData } from "common/models/contact";
 import * as Yup from "yup";
 import { useToast } from "dashboard/common/toast";
+import { TOAST_LIFETIME } from "common/settings";
+import { Status } from "common/models/base-response";
 const STEP = "step";
 
 export type PartialContact = Pick<
@@ -82,7 +84,17 @@ export const ContactForm = observer((): ReactElement => {
     useEffect(() => {
         const contactSections: any[] = [GeneralInfoData, ContactInfoData];
         if (id) {
-            getContact(id);
+            getContact(id).then((response) => {
+                if (response?.status === Status.ERROR) {
+                    toast.current?.show({
+                        severity: "error",
+                        summary: Status.ERROR,
+                        detail: (response?.error as string) || "",
+                        life: TOAST_LIFETIME,
+                    });
+                    navigate(`/dashboard/contacts`);
+                }
+            });
             contactSections.splice(2, 0, ContactMediaData);
         } else {
             clearContact();
@@ -140,6 +152,7 @@ export const ContactForm = observer((): ReactElement => {
                     severity: "error",
                     summary: "Validation Error",
                     detail: "Please fill in all required fields.",
+                    life: TOAST_LIFETIME,
                 });
             }
         });
