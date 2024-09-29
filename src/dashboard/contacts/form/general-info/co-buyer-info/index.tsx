238ac96331958dcdb95216a376a5d22ebb8f9104
danhalf <danhel@rocketmail.com>
2024-09-29 23:34:25 +0300

    added: co-buyer tab

--- src/dashboard/contacts/form/general-info/co-buyer-info/index.tsx
+++ src/dashboard/contacts/form/general-info/co-buyer-info/index.tsx
@@ -1,6 +1,27 @@
 import { observer } from "mobx-react-lite";
+import { TabView, TabPanel } from "primereact/tabview";
 import { ReactElement } from "react";
+import { ContactsAddressInfo } from "../tabs/address";
+import { ContactsGeneralInfo } from "../tabs/general";
+import { ContactsIdentificationInfo } from "../tabs/identification";
 
 export const ContactsCoBuyerInfo = observer((): ReactElement => {
-    return <>Co-buyer information TAB</>;
+    return (
+        <div className='col-12'>
+            <TabView className='contact-form__tabs'>
+                <TabPanel header='General'>
+                    <ContactsGeneralInfo type='buyer' />
+                </TabPanel>
+                <TabPanel header='Address'>
+                    <ContactsAddressInfo type='buyer' />
+                </TabPanel>
+                <TabPanel header='Identification'>
+                    <ContactsIdentificationInfo type='buyer' />
+                </TabPanel>
+                <TabPanel header='OFAC CHECK'>
+                    <div>OFAC CHECK TAB</div>
+                </TabPanel>
+            </TabView>
+        </div>
+    );
 });
