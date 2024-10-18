62be042e8ccbe19d97bb6da259fb8cb28712f45a
danhalf <danhalf90@gmail.com>
2024-10-18 19:10:16 +0300

    added: store active tab

--- src/dashboard/contacts/form/general-info/buyer-info/index.tsx
+++ src/dashboard/contacts/form/general-info/buyer-info/index.tsx
@@ -1,23 +1,28 @@
 import { observer } from "mobx-react-lite";
 import { TabPanel, TabView } from "primereact/tabview";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { ContactsAddressInfo } from "dashboard/contacts/form/general-info/tabs/address";
 import { ContactsGeneralInfo } from "dashboard/contacts/form/general-info/tabs/general";
 import { ContactsIdentificationInfo } from "dashboard/contacts/form/general-info/tabs/identification";
 import { ContactsOfacCheck } from "dashboard/contacts/form/general-info/tabs/ofac-check";
 import { GENERAL_CONTACT_TYPE } from "dashboard/contacts/form/general-info";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useStore } from "store/hooks";
 
 const { BUYER } = GENERAL_CONTACT_TYPE;
 
 export const ContactsBuyerInfo = observer((): ReactElement => {
+    const store = useStore().contactStore;
+    const { activeTab } = store;
     const navigate = useNavigate();
     const location = useLocation();
 
     const searchParams = new URLSearchParams(location.search);
     const tabParam = searchParams.get("tab");
 
-    const tabIndex = tabParam ? parseInt(tabParam, 10) : 0;
+    useEffect(() => {
+        store.activeTab = tabParam ? parseInt(tabParam, 10) : 0;
+    }, [tabParam]);
 
     const handleTabChange = (e: { index: number }) => {
         const newSearchParams = new URLSearchParams(location.search);
@@ -32,7 +37,7 @@ export const ContactsBuyerInfo = observer((): ReactElement => {
         <div className='col-12'>
             <TabView
                 className='contact-form__tabs'
-                activeIndex={tabIndex}
+                activeIndex={activeTab || 0}
                 onTabChange={handleTabChange}
             >
                 <TabPanel header='General'>
