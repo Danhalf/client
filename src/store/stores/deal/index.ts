b18c7972f9e1db2eb68b42be5682713342cd0303
danhalf <danhel@rocketmail.com>
2025-03-17 18:02:23 +0200

    FEAT: add inventory data request

--- src/store/stores/deal/index.ts
+++ src/store/stores/deal/index.ts
@@ -6,6 +6,7 @@ import {
     DealPickupPayment,
     DealPrintForm,
 } from "common/models/deals";
+import { Inventory } from "common/models/inventory";
 import {
     getDealFinance,
     getDealInfo,
@@ -15,6 +16,7 @@ import {
     setDealFinance,
     setDealPayments,
 } from "http/services/deals.service";
+import { getInventoryInfo } from "http/services/inventory-service";
 import { action, makeAutoObservable } from "mobx";
 import { RootStore } from "store";
 
@@ -27,6 +29,7 @@ interface DealPrintCollection {
 export class DealStore {
     public rootStore: RootStore;
     private _deal: DealItem = {} as DealItem;
+    private _inventory: Inventory = {} as Inventory;
     private _dealExtData: DealExtData = {} as DealExtData;
     private _dealFinance = {} as DealFinance;
     private _dealFinances: DealFinance = {} as DealFinance;
@@ -48,6 +51,11 @@ export class DealStore {
     public get deal() {
         return this._deal;
     }
+
+    public get inventory() {
+        return this._inventory;
+    }
+
     public get dealExtData() {
         return this._dealExtData;
     }
@@ -104,6 +112,10 @@ export class DealStore {
                 this._dealID = extdata.dealUID;
                 this._dealExtData = extdata || ({} as DealExtData);
                 this._dealFinance = finance || ({} as DealFinance);
+                const inventoryResponse = await getInventoryInfo(deal.inventoryuid);
+                if (inventoryResponse?.status === Status.OK) {
+                    this._inventory = inventoryResponse as Inventory;
+                }
             } else {
                 const { error } = response as BaseResponseError;
                 this._dealErrorMessage = error!;
