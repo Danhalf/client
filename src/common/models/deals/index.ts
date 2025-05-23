5f24a28b283cf5e76845d2cbaa6fd59dba297fc0
danhalf <danhel@rocketmail.com>
2025-05-23 20:20:10 +0300

    FEAT: add DealPaymentsTotal interface

--- src/common/models/deals/index.ts
+++ src/common/models/deals/index.ts
@@ -1,4 +1,4 @@
-import { BaseResponse } from "common/models/base-response";
+import { BaseResponse, BaseResponseError } from "common/models/base-response";
 import { PrintForm, TypeList } from "..";
 import { Status } from "../base-response";
 
@@ -437,4 +437,9 @@ export interface DealDelete {
     delete_trade1: 0 | 1;
     delete_trade2: 0 | 1;
     delete_account: 0 | 1;
+}
+
+export interface DealPaymentsTotal extends BaseResponseError {
+    total: number;
+    total_paid: number;
 }
