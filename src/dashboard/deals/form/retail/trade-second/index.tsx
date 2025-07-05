3d2c35cf1d64e0e2cac74582bd8f5a13269aeb94
danhalf <danhel@rocketmail.com>
2025-07-05 23:14:47 +0300

    FEAT: add checked state and empty date handling to DateInput in DealRetailTradeSecond component

--- src/dashboard/deals/form/retail/trade-second/index.tsx
+++ src/dashboard/deals/form/retail/trade-second/index.tsx
@@ -465,6 +465,7 @@ export const DealRetailTradeSecond = observer((): ReactElement => {
                 <DateInput
                     date={Trade2_Lien_Payoff_Good_Through}
                     checkbox
+                    checked={!!Trade2_Lien_Payoff_Good_Through}
                     onChange={({ value }) =>
                         value &&
                         changeDealExtData({
@@ -472,6 +473,7 @@ export const DealRetailTradeSecond = observer((): ReactElement => {
                             value: Number(value),
                         })
                     }
+                    emptyDate
                     name='PO Good Thru'
                 />
             </div>
