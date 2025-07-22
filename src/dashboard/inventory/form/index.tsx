ce07acac88c4bf924d3775c730edc411b434f34a
danhalf <danhel@rocketmail.com>
2025-07-22 19:51:29 +0300

    REF:  refactor VIN validation calls in VehicleGeneral component

--- src/dashboard/inventory/form/index.tsx
+++ src/dashboard/inventory/form/index.tsx
@@ -184,8 +184,7 @@ export const InventoryForm = observer(() => {
                 } catch (error) {
                     resolve(true);
                 }
-            },
-            500
+            }
         );
 
         const debouncedCheckVINAvailability = debounce(
