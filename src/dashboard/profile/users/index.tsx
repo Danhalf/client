7a42d735e5296f8af98cc283ddf5076a5d89ccba
danhalf <danhel@rocketmail.com>
2025-10-13 20:37:07 +0300

    FEAT: implement Users component with user fetching logic and integrate UsersStore

--- src/dashboard/profile/users/index.tsx
+++ src/dashboard/profile/users/index.tsx
@@ -1,3 +1,37 @@
-export const Users = () => {
-    return <div>Users</div>;
-};
+import { getClientsList, getUsersList } from "http/services/users";
+import { observer } from "mobx-react-lite";
+import { useStore } from "store/hooks";
+import { useEffect, useState } from "react";
+
+export const Users = observer(() => {
+    const { authUser } = useStore().userStore;
+
+    const [users, setUsers] = useState<any[]>([]);
+
+    const handleGetUsers = async () => {
+        if (!authUser) return;
+        const response = await getUsersList(authUser?.useruid);
+        if (response && Array.isArray(response)) {
+            return setUsers(response);
+        }
+        const clientsResponse = await getClientsList(authUser?.useruid);
+        if (clientsResponse && Array.isArray(clientsResponse)) {
+            return setUsers(clientsResponse);
+        }
+    };
+
+    useEffect(() => {
+        handleGetUsers();
+    }, []);
+
+    return (
+        <div>
+            <h1>Users</h1>
+            <div>
+                {users.map((user) => (
+                    <div key={user.useruid}>{user.username}</div>
+                ))}
+            </div>
+        </div>
+    );
+});
