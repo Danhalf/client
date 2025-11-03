b5eeb337effec1efcce23bc9d26989e6d2948961
danhalf <danhel@rocketmail.com>
2025-11-03 20:34:01 +0200

    FEAT: simplify sidebar and main layout styles, add sidebar overlay, and streamline header component

--- src/dashboard/sidebar/index.tsx
+++ src/dashboard/sidebar/index.tsx
@@ -3,10 +3,7 @@ import { Link } from "react-router-dom";
 import { useState, useEffect, ReactElement } from "react";
 import { useStore } from "store/hooks";
 import { observer } from "mobx-react-lite";
-import { Button } from "primereact/button";
 import { Tooltip } from "primereact/tooltip";
-import { getUserSettings, setUserSettings } from "http/services/auth-user.service";
-import { ServerUserSettings } from "common/models/user";
 import {
     ACCOUNTS_PAGE,
     CONTACTS_PAGE,
@@ -20,47 +17,22 @@ import {
 
 export const Sidebar = observer((): ReactElement => {
     const store = useStore().userStore;
-    const { authUser, settings, isSettingsLoaded } = store;
+    const { authUser, settings } = store;
     const [isSalesPerson, setIsSalesPerson] = useState(true);
-    const [serverSettings, setServerSettings] = useState<ServerUserSettings | undefined>(undefined);
 
     useEffect(() => {
         if (authUser) {
-            getUserSettings(authUser.useruid).then((response) => {
-                if (response?.profile?.length) {
-                    let allSettings: ServerUserSettings = {} as ServerUserSettings;
-                    try {
-                        allSettings = JSON.parse(response.profile);
-                    } catch (error) {
-                        allSettings = {} as ServerUserSettings;
-                    }
-                    setServerSettings(allSettings);
-                    if (allSettings?.sidebar?.isSidebarCollapsed !== undefined) {
-                        settings.isSidebarCollapsed = !allSettings.sidebar.isSidebarCollapsed;
-                    }
-                }
-                store.isSettingsLoaded = true;
-            });
+            settings.isSidebarCollapsed = true;
+            store.isSettingsLoaded = true;
         }
-    }, [settings]);
+    }, [authUser]);
 
-    const changeSettings = (newSidebarSettings: { isSidebarCollapsed: boolean }) => {
-        if (authUser && serverSettings !== undefined) {
-            const updatedSettings: ServerUserSettings = {
-                ...serverSettings,
-                sidebar: {
-                    ...serverSettings?.sidebar,
-                    ...newSidebarSettings,
-                },
-            };
-            setServerSettings(updatedSettings);
-            setUserSettings(authUser.useruid, updatedSettings);
-        }
+    const handleMouseEnter = () => {
+        settings.isSidebarCollapsed = false;
     };
 
-    const handleToggleSidebar = () => {
-        settings.toggleSidebar();
-        changeSettings({ isSidebarCollapsed: !settings.isSidebarCollapsed });
+    const handleMouseLeave = () => {
+        settings.isSidebarCollapsed = true;
     };
 
     useEffect(() => {
@@ -89,27 +61,12 @@ export const Sidebar = observer((): ReactElement => {
         );
     };
 
-    if (!isSettingsLoaded) {
-        return <></>;
-    }
-
     return (
         <aside
             className={`sidebar hidden lg:block ${settings.isSidebarCollapsed ? "collapsed" : ""}`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
         >
-            <Button
-                className='sidebar-toggle'
-                onClick={handleToggleSidebar}
-                rounded
-                icon='pi pi-angle-left'
-                aria-label={`${settings.isSidebarCollapsed ? "Expand" : "Collapse"} sidebar`}
-                tooltip={`${settings.isSidebarCollapsed ? "Expand" : "Collapse"} sidebar`}
-                pt={{
-                    icon: {
-                        className: `${settings.isSidebarCollapsed ? "pi pi-angle-right" : "pi pi-angle-left"}`,
-                    },
-                }}
-            />
             <ul className='sidebar-nav'>
                 {renderNavItem(DASHBOARD_PAGE, "home", "Home")}
                 {renderNavItem(INVENTORY_PAGE.MAIN, "inventory", "Inventory")}
