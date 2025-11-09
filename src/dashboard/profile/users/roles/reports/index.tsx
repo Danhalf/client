8fdf700f512065c4acf8cb9717b6ec9a4c824d5b
danhalf <danhel@rocketmail.com>
2025-11-09 22:55:16 +0200

    FEAT: add role management checkboxes for reports and settings in RolesReports and RolesSettings components

--- src/dashboard/profile/users/roles/reports/index.tsx
+++ src/dashboard/profile/users/roles/reports/index.tsx
@@ -1,10 +1,42 @@
+import { BorderedCheckbox } from "dashboard/common/form/inputs";
 import { observer } from "mobx-react-lite";
-import { ReactElement } from "react";
+import { CheckboxChangeEvent } from "primereact/checkbox";
+import { ReactElement, useState } from "react";
 
 export const RolesReports = observer((): ReactElement => {
+    const [selectAll, setSelectAll] = useState<boolean>(false);
+    const [allowReports, setAllowReports] = useState<boolean>(false);
+    const [createReports, setCreateReports] = useState<boolean>(false);
+
+    const handleSelectAllChange = (event: CheckboxChangeEvent) => {
+        setSelectAll(event.checked ?? false);
+        setAllowReports(event.checked ?? false);
+        setCreateReports(event.checked ?? false);
+    };
+
     return (
-        <div>
-            <div>Reports roles settings</div>
-        </div>
+        <section className='grid roles-reports'>
+            <div className='col-3'>
+                <BorderedCheckbox
+                    name='Select All'
+                    checked={selectAll}
+                    onChange={handleSelectAllChange}
+                />
+            </div>
+            <div className='col-3'>
+                <BorderedCheckbox
+                    name='Allow Reports'
+                    checked={allowReports}
+                    onChange={() => setAllowReports(!allowReports)}
+                />
+            </div>
+            <div className='col-3'>
+                <BorderedCheckbox
+                    name='Create Reports'
+                    checked={createReports}
+                    onChange={() => setCreateReports(!createReports)}
+                />
+            </div>
+        </section>
     );
 });
