4ffce5c20da81e94ab752e959d48f77e0499fef0
danhalf <danhel@rocketmail.com>
2025-11-11 19:00:18 +0200

    FEAT: refactor RolesContacts and RolesDeals components to utilize PermissionKey type for permission management and streamline checkbox handling

--- src/dashboard/profile/users/roles/form/contacts/index.tsx
+++ src/dashboard/profile/users/roles/form/contacts/index.tsx
@@ -1,21 +1,32 @@
+import { PermissionKey } from "common/constants/permissions";
 import { BorderedCheckbox } from "dashboard/common/form/inputs";
 import { observer } from "mobx-react-lite";
 import { CheckboxChangeEvent } from "primereact/checkbox";
 import { ReactElement, useState } from "react";
+import { useStore } from "store/hooks";
 
 export const RolesContacts = observer((): ReactElement => {
+    const { togglePermission, hasRolePermission } = useStore().usersStore;
     const [selectAll, setSelectAll] = useState<boolean>(false);
-    const [addContacts, setAddContacts] = useState<boolean>(false);
-    const [editContacts, setEditContacts] = useState<boolean>(false);
-    const [viewContacts, setViewContacts] = useState<boolean>(false);
-    const [deleteContacts, setDeleteContacts] = useState<boolean>(false);
 
     const handleSelectAllChange = (event: CheckboxChangeEvent) => {
         setSelectAll(event.checked ?? false);
-        setAddContacts(event.checked ?? false);
-        setEditContacts(event.checked ?? false);
-        setViewContacts(event.checked ?? false);
-        setDeleteContacts(event.checked ?? false);
+        const contactsPermissions: readonly PermissionKey[] = [
+            "uaAddContacts",
+            "uaEditContacts",
+            "uaViewContacts",
+            "uaDeleteContacts",
+        ];
+
+        contactsPermissions.forEach((permission: PermissionKey) => {
+            const shouldAdd = event.checked ?? false;
+            const hasPermission = hasRolePermission(permission);
+            if (shouldAdd && !hasPermission) {
+                togglePermission(permission);
+            } else if (!shouldAdd && hasPermission) {
+                togglePermission(permission);
+            }
+        });
     };
 
     return (
@@ -30,29 +41,29 @@ export const RolesContacts = observer((): ReactElement => {
             <div className='col-3'>
                 <BorderedCheckbox
                     name='Add Contacts'
-                    checked={addContacts}
-                    onChange={() => setAddContacts(!addContacts)}
+                    checked={hasRolePermission("uaAddContacts")}
+                    onChange={() => togglePermission("uaAddContacts")}
                 />
             </div>
             <div className='col-3'>
                 <BorderedCheckbox
                     name='Edit Contacts'
-                    checked={editContacts}
-                    onChange={() => setEditContacts(!editContacts)}
+                    checked={hasRolePermission("uaEditContacts")}
+                    onChange={() => togglePermission("uaEditContacts")}
                 />
             </div>
             <div className='col-3'>
                 <BorderedCheckbox
                     name='View Contacts'
-                    checked={viewContacts}
-                    onChange={() => setViewContacts(!viewContacts)}
+                    checked={hasRolePermission("uaViewContacts")}
+                    onChange={() => togglePermission("uaViewContacts")}
                 />
             </div>
             <div className='col-3'>
                 <BorderedCheckbox
                     name='Delete Contacts'
-                    checked={deleteContacts}
-                    onChange={() => setDeleteContacts(!deleteContacts)}
+                    checked={hasRolePermission("uaDeleteContacts")}
+                    onChange={() => togglePermission("uaDeleteContacts")}
                 />
             </div>
         </section>
