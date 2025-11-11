b6664d0c3eb6df9727efcbf31a6f11ae8769790d
danhalf <danhel@rocketmail.com>
2025-11-11 19:05:22 +0200

    FEAT: refactor RolesInventory and RolesOther components to utilize PermissionKey type for permission management and streamline checkbox handling

--- src/dashboard/profile/users/roles/form/other/index.tsx
+++ src/dashboard/profile/users/roles/form/other/index.tsx
@@ -1,23 +1,27 @@
+import { PermissionKey } from "common/constants/permissions";
 import { BorderedCheckbox } from "dashboard/common/form/inputs";
 import { observer } from "mobx-react-lite";
 import { CheckboxChangeEvent } from "primereact/checkbox";
 import { ReactElement, useState } from "react";
+import { useStore } from "store/hooks";
 
 export const RolesOther = observer((): ReactElement => {
+    const { togglePermission, hasRolePermission } = useStore().usersStore;
     const [selectAll, setSelectAll] = useState<boolean>(false);
-    const [allowMobile, setAllowMobile] = useState<boolean>(false);
-    const [allowPrinting, setAllowPrinting] = useState<boolean>(false);
-    const [allowWeb, setAllowWeb] = useState<boolean>(false);
-    const [viewDeleted, setViewDeleted] = useState<boolean>(false);
-    const [undeleteDeleted, setUndeleteDeleted] = useState<boolean>(false);
 
     const handleSelectAllChange = (event: CheckboxChangeEvent) => {
         setSelectAll(event.checked ?? false);
-        setAllowMobile(event.checked ?? false);
-        setAllowPrinting(event.checked ?? false);
-        setAllowWeb(event.checked ?? false);
-        setViewDeleted(event.checked ?? false);
-        setUndeleteDeleted(event.checked ?? false);
+        const otherPermissions: readonly PermissionKey[] = ["uaAllowReports", "uaAllowPrinting"];
+
+        otherPermissions.forEach((permission: PermissionKey) => {
+            const shouldAdd = event.checked ?? false;
+            const hasPermission = hasRolePermission(permission);
+            if (shouldAdd && !hasPermission) {
+                togglePermission(permission);
+            } else if (!shouldAdd && hasPermission) {
+                togglePermission(permission);
+            }
+        });
     };
     return (
         <section className='grid roles-other'>
@@ -29,39 +33,23 @@ export const RolesOther = observer((): ReactElement => {
                 />
             </div>
             <div className='col-3'>
-                <BorderedCheckbox
-                    name='Allow Mobile'
-                    checked={allowMobile}
-                    onChange={() => setAllowMobile(!allowMobile)}
-                />
+                <BorderedCheckbox name='Allow Mobile' checked disabled />
             </div>
             <div className='col-3'>
                 <BorderedCheckbox
                     name='Allow Printing'
-                    checked={allowPrinting}
-                    onChange={() => setAllowPrinting(!allowPrinting)}
+                    checked={hasRolePermission("uaAllowPrinting")}
+                    onChange={() => togglePermission("uaAllowPrinting")}
                 />
             </div>
             <div className='col-3'>
-                <BorderedCheckbox
-                    name='Allow Web'
-                    checked={allowWeb}
-                    onChange={() => setAllowWeb(!allowWeb)}
-                />
+                <BorderedCheckbox name='Allow Web' checked disabled />
             </div>
             <div className='col-3'>
-                <BorderedCheckbox
-                    name='View Deleted'
-                    checked={viewDeleted}
-                    onChange={() => setViewDeleted(!viewDeleted)}
-                />
+                <BorderedCheckbox name='View Deleted' checked disabled />
             </div>
             <div className='col-3'>
-                <BorderedCheckbox
-                    name='Undelete Deleted'
-                    checked={undeleteDeleted}
-                    onChange={() => setUndeleteDeleted(!undeleteDeleted)}
-                />
+                <BorderedCheckbox name='Undelete Deleted' checked disabled />
             </div>
         </section>
     );
