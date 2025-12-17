e834195f7339c7dbea0782301ab8a6e3583b9f8c
danhalf <danhalf90@gmail.com>
2025-12-17 14:33:28 +0200

    FEAT: block inventory group reordering during server requests

---
+++ src/common/constants/report-options.ts
@@ -0,0 +1,11 @@
+export enum MOVE_DIRECTION {
+    LEFT = "left",
+    RIGHT = "right",
+    TOP = "top",
+    BOTTOM = "bottom",
+    UP = "up",
+    DOWN = "down",
+}
+
+export const MOVE_ID: string = "move";
+export const NEW_ITEM: string = "new";
