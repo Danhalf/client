ed3e9196693449d19b071f6a5246ea052a05f6ce
danhalf <danhalf90@gmail.com>
2026-01-13 18:35:42 +0200

    REF: update class names / layout

--- src/dashboard/profile/userProfile/personal-information/index.tsx
+++ src/dashboard/profile/userProfile/personal-information/index.tsx
@@ -1,45 +1,110 @@
-import { ReactElement } from "react";
-import { InputText } from "primereact/inputtext";
+import { ReactElement, useState } from "react";
 import { useStore } from "store/hooks";
 import { observer } from "mobx-react-lite";
+import { PhoneInput, StateDropdown, TextInput } from "dashboard/common/form/inputs";
+import "./index.css";
+import { Splitter } from "dashboard/common/display";
 
 export const PersonalInformation = observer((): ReactElement => {
     const store = useStore().userStore;
     const { authUser } = store;
 
+    const [username, setUsername] = useState<string>(authUser?.loginname || "");
+    const [companyName, setCompanyName] = useState<string>(authUser?.companyname || "");
+    const [location, setLocation] = useState<string>(authUser?.locationname || "");
+    const [address, setAddress] = useState<string>("");
+    const [state, setState] = useState<string>("");
+    const [zipCode, setZipCode] = useState<string>("");
+    const [phoneNumber, setPhoneNumber] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+
     return (
         <div className='user-profile__content'>
-            <div className='user-profile__row profile-row'>
-                <div className='profile-row__label'>User name</div>
-                <div className='profile-row__value'>{authUser?.loginname}</div>
+            <div className='user-profile__header'>
+                <h3 className='user-profile__section-title'>Personal Information</h3>
             </div>
-            <div className='user-profile__row profile-row'>
-                <div className='profile-row__label'>Company name</div>
-                <div className='profile-row__value'>
-                    <InputText placeholder='Company name' value={authUser?.companyname || ""} />
+            <div className='user-profile-personal grid'>
+                <div className='col-6 user-profile-personal__info'>
+                    <TextInput
+                        name='Username'
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
+                    <TextInput
+                        name='Company Name'
+                        value={companyName}
+                        onChange={(event) => setCompanyName(event.target.value)}
+                    />
+                    <TextInput
+                        name='Location (city/state)'
+                        value={location}
+                        onChange={(event) => setLocation(event.target.value)}
+                    />
+                </div>
+                <div className='col-6 user-profile-personal__avatar'>
+                    <div className='user-profile-avatar'>
+                        <div className='user-profile-avatar__placeholder'>
+                            <i className='pi pi-user' />
+                        </div>
+                        <div className='user-profile-avatar__choose'>
+                            <i className='pi pi-camera' />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className='user-profile__row profile-row'>
-                <div className='profile-row__label'>Location</div>
-                <div className='profile-row__value'>
-                    <InputText placeholder='Location' value={authUser?.locationname || ""} />
+
+            <Splitter className='col-12' />
+
+            <div className='user-profile-contact grid'>
+                <div className='col-6'>
+                    <TextInput
+                        name='Location (address)'
+                        value={address}
+                        onChange={(event) => setAddress(event.target.value)}
+                    />
                 </div>
-            </div>
-            <div className='user-profile__row profile-row'>
-                <div className='profile-row__label'>Address</div>
-                <div className='profile-row__value'>
-                    <InputText placeholder='Address' value={""} />
+
+                <div className='col-6'>
+                    <StateDropdown
+                        name='State'
+                        showClear={!!state}
+                        className='w-full'
+                        value={state}
+                        onChange={({ value }) => setState(value || "")}
+                    />
                 </div>
-            </div>
-            <div className='user-profile__row profile-row'>
-                <div className='profile-row__label'>Phone</div>
-                <div className='profile-row__value'>
-                    <InputText placeholder='Phone' value={""} />
+                <div className='col-6'>
+                    <TextInput
+                        name='Zip Code'
+                        value={zipCode}
+                        onChange={(event) => setZipCode(event.target.value)}
+                    />
                 </div>
-            </div>
-            <div className='user-profile__row profile-row'>
-                <div className='profile-row__label'>Last login</div>
-                <div className='profile-row__value'>04/26/2023 10:45:39 PM</div>
+                <div className='col-6'>
+                    <PhoneInput
+                        name='Phone Number'
+                        required
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                </div>
+                <div className='col-6'>
+                    <TextInput
+                        name='E-mail'
+                        type='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className='col-12 user-profile-info-message'>
+                    <i className='pi pi-info-circle user-profile-info-message__icon' />
+                    <span className='user-profile-info-message__text'>
+                        At least one contact method is required - phone number or email. Without
+                        this information, two-factor authentication cannot be set up for the user in
+                        the future. If both fields are filled in, the user will be able to choose
+                        their preferred two-factor authentication method.
+                    </span>
+                </div>
             </div>
         </div>
     );
--- src/dashboard/profile/userProfile/security/index.tsx
+++ src/dashboard/profile/userProfile/security/index.tsx
@@ -1,5 +1,6 @@
 import { ReactElement, useState } from "react";
 import { Password } from "primereact/password";
+import "./index.css";
 
 export const Security = (): ReactElement => {
     const [showChangePassword, setShowChangePassword] = useState<boolean>(false);
@@ -11,53 +12,45 @@ export const Security = (): ReactElement => {
         <div className='user-profile__content'>
             <div className='user-profile__section'>
                 <h3 className='user-profile__section-title'>CHANGE PASSWORD</h3>
-                {!showChangePassword && (
-                    <div className='user-profile__row profile-row'>
-                        <div
-                            className='profile__change-password'
-                            onClick={() => setShowChangePassword(true)}
-                        >
-                            Change password
+                <div className='user-profile-password'>
+                    {!showChangePassword && (
+                        <div className='user-profile-password__trigger'>
+                            <span
+                                className='user-profile-password__link'
+                                onClick={() => setShowChangePassword(true)}
+                            >
+                                Change password
+                            </span>
                         </div>
-                    </div>
-                )}
-                {showChangePassword && (
-                    <>
-                        <div className='user-profile__row profile-row'>
-                            <div className='profile-row__label'>Current Password</div>
-                            <div className='profile-row__value'>
+                    )}
+                    {showChangePassword && (
+                        <div className='user-profile-password__fields'>
+                            <div className='user-profile-password__field'>
                                 <Password
-                                    className='w-100'
+                                    className='user-profile-password__input'
                                     value={currentPassword}
                                     onChange={(event) => setCurrentPassword(event.target.value)}
                                     toggleMask
                                     placeholder='Current Password'
                                 />
                             </div>
-                        </div>
-                        <div className='user-profile__row profile-row'>
-                            <div className='profile-row__label'></div>
-                            <div className='profile-row__value'>
-                                <span className='profile__forgot-password'>Forgot password?</span>
+                            <div className='user-profile-password__forgot'>
+                                <span className='user-profile-password__forgot-link'>
+                                    Forgot password?
+                                </span>
                             </div>
-                        </div>
-                        <div className='user-profile__row profile-row'>
-                            <div className='profile-row__label'>New Password</div>
-                            <div className='profile-row__value'>
+                            <div className='user-profile-password__field'>
                                 <Password
-                                    className='w-100'
+                                    className='user-profile-password__input'
                                     value={newPassword}
                                     onChange={(event) => setNewPassword(event.target.value)}
                                     toggleMask
                                     placeholder='New Password'
                                 />
                             </div>
-                        </div>
-                        <div className='user-profile__row profile-row'>
-                            <div className='profile-row__label'>Confirm Password</div>
-                            <div className='profile-row__value'>
+                            <div className='user-profile-password__field'>
                                 <Password
-                                    className='w-100'
+                                    className='user-profile-password__input'
                                     value={confirmPassword}
                                     onChange={(event) => setConfirmPassword(event.target.value)}
                                     toggleMask
@@ -65,17 +58,21 @@ export const Security = (): ReactElement => {
                                 />
                             </div>
                         </div>
-                    </>
-                )}
+                    )}
+                </div>
             </div>
             <div className='user-profile__section'>
                 <h3 className='user-profile__section-title'>TWO-FACTOR AUTHENTICATION</h3>
-                <p className='user-profile__section-text'>
-                    For security reasons, two-factor authentication (2FA) cannot be disabled. If you
-                    need to <strong>reset</strong> your 2FA settings, please{" "}
-                    <span className='user-profile__link'>contact our support team</span> for
-                    assistance. We're here to help ensure your account remains secure.
-                </p>
+                <div className='user-profile-two-factor'>
+                    <p className='user-profile-two-factor__text'>
+                        For security reasons, two-factor authentication (2FA) cannot be disabled. If
+                        you need to <strong>reset</strong> your 2FA settings, please{" "}
+                        <span className='user-profile-two-factor__link'>
+                            contact our support team
+                        </span>{" "}
+                        for assistance. We're here to help ensure your account remains secure.
+                    </p>
+                </div>
             </div>
         </div>
     );
