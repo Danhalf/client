013399d770437fda9f4cb45fc25d84c647fdf741
danhalf <danhalf90@gmail.com>
2026-02-02 14:28:19 +0200

    FIX: update main branch / remove checktoken endpoint

--- src/dashboard/header/index.tsx
+++ src/dashboard/header/index.tsx
@@ -14,7 +14,13 @@ import { SupportHistoryDialog } from "dashboard/profile/supportHistory";
 import { useStore } from "store/hooks";
 import { observer } from "mobx-react-lite";
 import { getExtendedData } from "http/services/auth-user.service";
-import { CONTACT_SUPPORT, HELP_PAGE, SETTINGS_PAGE, USERS_PAGE, USER_PROFILE_PAGE } from "common/constants/links";
+import {
+    CONTACT_SUPPORT,
+    HELP_PAGE,
+    SETTINGS_PAGE,
+    USERS_PAGE,
+    USER_PROFILE_PAGE,
+} from "common/constants/links";
 
 export const Header = observer((): ReactElement => {
     const store = useStore().userStore;
