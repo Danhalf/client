49e6d35dcc8127a0a057d8a54fbc3c35e197355b
danhalf <danhel@rocketmail.com>
2026-03-08 21:54:08 +0200

    FEAT: add refresh token functionality request

--- src/common/models/user/index.ts
+++ src/common/models/user/index.ts
@@ -215,6 +215,21 @@ export interface TwoFactorCheckEndpointRequest {
     endpoint: string;
 }
 
+export interface RefreshTokenRequest {
+    refresh_token: string;
+    sessionuid: string;
+}
+
+export interface RefreshTokenResponse extends BaseResponseError {
+    token: string;
+    token_type: string;
+    expires_in: number;
+    refresh_token: string;
+    refresh_token_expires_in: number;
+    sessionuid: string;
+    useruid: string;
+}
+
 export type UserPermissionsResponse = BaseResponseError & {
     [K in PermissionKey]: 0 | 1;
 };
